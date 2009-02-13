67f0cd57bd88eb3f93ecac869d057fff7b8faf4a
cavedude00 <cavedude00@e2836f04-be38-11dd-ab14-3be0eea8eb7e>
2009-02-13 06:24:54 +0000

    All spell searches will now use spells_new. Fixed the bug where a searched spell would not autofill the field when clicked.

--- templates/spellset/spell.add.tmpl.php
+++ templates/spellset/spell.add.tmpl.php
@@ -10,31 +10,31 @@
         <div class="edit_form_content">
           <form method="post" action="index.php?editor=spellset&z=<?=$currzone?>&npcid=<?=$npcid?>&spellset=<?=$spellset?>&action=4">
             <strong>Spell ID:</strong> (<a href="javascript:showSearch();">search</a>)<br>
-            <input id="spellid" type="text" name="spellid" size="10" value=""><br><br>
+            <input class="indented" id="id" type="text" name="spellid" size="10" value="0"><br><br>
 
             <strong>Type:</strong> <br>
-            <select name="type">
+            <select class = "indented" name="type">
 <?foreach($spelltypes as $k => $v):?>
               <option value="<?=$k?>"><?=$v?></option>
 <?endforeach;?>
             </select><br><br>
 
             <strong>Minlevel:</strong><br>
-            <input type="text" name="minlevel" size="10" value="1"><br><br>
+            <input class="indented" id="id" input type="text" name="minlevel" size="10" value="1"><br><br>
 
             <strong>Maxlevel:</strong><br>
-            <input type="text" name="maxlevel" size="10" value="255"><br><br>
+            <input class="indented" id="id" input type="text" name="maxlevel" size="10" value="255"><br><br>
 
             <strong>Mana Cost:</strong><br>
-            <input type="text" name="manacost" size="10" value="-1"><br>
+            <input class="indented" id="id" input type="text" name="manacost" size="10" value="-1"><br>
             (-1 = Default)<br><br>
 
             <strong>Recast Delay:</strong><br>
-            <input type="text" name="recast_delay" size="10" value="-1"><br>
+            <input class="indented" id="id" input type="text" name="recast_delay" size="10" value="-1"><br>
             (-1 = Default)<br><br>
 
             <strong>Priority:</strong><br>
-            <input type="text" name="priority" size="10" value="0"><br><br>
+            <input class="indented" id="id" input type="text" name="priority" size="10" value="0"><br><br>
 
             <center>
               <input type="hidden" name="npc_spells_id" value="<?=$npc_spells_id?>">
