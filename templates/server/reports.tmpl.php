03439a78f6a555910fa5733a69eb5e9049ff75c3
cavedude00 <cavedude00@e2836f04-be38-11dd-ab14-3be0eea8eb7e>
2009-09-03 22:38:44 +0000

    Minor visual fix

--- templates/server/reports.tmpl.php
+++ templates/server/reports.tmpl.php
@@ -1,4 +1,4 @@
-      <div class="table_container" style="width: 400px;">
+      <div class="table_container" style="width: 650px;">
       <div class="table_header">
         <table width="100%" cellpadding="0" cellspacing="0">
           <tr>
@@ -13,15 +13,15 @@
 <? if (isset($reports)) :?>
          <tr>
           <td align="center" width="5%"><strong>ID</strong></td>
-          <td align="center" width="5%"><strong>Name</strong></td>
-          <td align="center" width="5%"><strong>Reported</strong></td>
+          <td align="center" width="15%"><strong>Name</strong></td>
+          <td align="center" width="70%"><strong>Reported</strong></td>
           <th width="5%"></th>
          </tr>
       <?$x=0; foreach($reports as $reports=>$v):?>
         <tr bgcolor="#<? echo ($x % 2 == 0) ? "AAAAAA" : "BBBBBB";?>">
           <td align="center" width="5%"><?=$v['rid']?></td>
-          <td align="center" width="5%"><?=$v['name']?></td>
-          <td align="center" width="5%"><?=$v['reported']?></td>  
+          <td align="center" width="15%"><?=$v['name']?></td>
+          <td align="center" width="70%"><?=$v['reported']?></td>  
           <td align="right">      
             <a href="index.php?editor=server&rid=<?=$v['rid']?>&action=11"><img src="images/edit2.gif" border="0" title="View Hacker"></a>            
             <a onClick="return confirm('Really Delete Entry <?=$v['rid']?>?');" href="index.php?editor=server&rid=<?=$v['rid']?>&action=10"><img src="images/remove3.gif" border="0" title="Delete this entry"></a>
