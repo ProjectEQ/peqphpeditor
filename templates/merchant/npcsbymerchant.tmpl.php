7299d812197e5ec5e0a97474a83c92c7368de594
cavedude00 <cavedude00@e2836f04-be38-11dd-ab14-3be0eea8eb7e>
2009-09-26 16:37:27 +0000

    Fix for merchant search result wrapping.

--- templates/merchant/npcsbymerchant.tmpl.php
+++ templates/merchant/npcsbymerchant.tmpl.php
@@ -1,11 +1,11 @@
-      <div class="table_container" style="width: 200px">
+      <div class="table_container" style="width: 250px">
       <div class="table_header">
            <td>NPCs using Merchantlist <?=$merid?></td>
       </div>
        <div class="table_content">
 <?if ($merlist != ''):?>
   <?foreach($merlist as $merlist):?>
-            <td align="center" width="5%"> <a href="index.php?editor=npc&z=<?=get_zone_by_npcid($merlist['npcid'])?>&npcid=<?=$merlist['npcid']?>"> <?=$merlist['name']?> - (<?=get_zone_by_npcid($merlist['npcid'])?>)</td>
+            <td align="center" width="5%"> <a href="index.php?editor=npc&z=<?=get_zone_by_npcid($merlist['npcid'])?>&npcid=<?=$merlist['npcid']?>"> <?=$merlist['name']?> - (<?=get_zone_by_npcid($merlist['npcid'])?>)</td><br>
  <?endforeach;?>
 <?endif;?>
 <?if ($merlist == ''):?>
