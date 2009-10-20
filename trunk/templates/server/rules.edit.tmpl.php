b2dc7cb67c98870750b6292226e546fe495a90e5
cavedude00 <cavedude00@e2836f04-be38-11dd-ab14-3be0eea8eb7e>
2009-10-20 19:52:42 +0000

    Changes to rules. Fixed a typo in hackers.

--- trunk/templates/server/rules.edit.tmpl.php
+++ trunk/templates/server/rules.edit.tmpl.php
@@ -3,7 +3,7 @@
         Edit Rule
       </div>
       <div class="edit_form_content">
-        <form name="rules" method="post" action=index.php?editor=server&action=18">
+        <form name="rules" method="post" action=index.php?editor=server&action=18>
         <table width="100%">
           <tr>
             <th>Ruleset</th>
@@ -12,7 +12,7 @@
             <th>Notes</th>
           </tr>
           <tr>
-            <td><input type="text" size="4" name="ruleset_id" value="<?=$ruleset_id?>"></td>
+            <td><input type="text" size="4" name="ruleset_id1" value="<?=$ruleset_id?>"></td>
             <td><input type="text" size="47" name="rule_name1" value="<?=$rule_name?>"></td>
             <td><input type="text" size="8" name="rule_value" value="<?=$rule_value?>"></td>
             <td><input type="text" size="42" name="notes" value="<?=$notes?>"></td>
@@ -20,6 +20,7 @@
               </table><br><br>
         <center>
           <input type="hidden" name="rule_name" value="<?=$rule_name?>">
+          <input type="hidden" name="ruleset_id" value="<?=$ruleset_id?>">
           <input type="submit" value="Submit Changes">
         </center>
       </form>
---
+++ trunk/templates/server/ruleset.switch.tmpl.php
@@ -0,0 +1,33 @@
+      <div class="table_container" style="width: 200px;">
+      <div class="table_header">
+        <table width="100%" cellpadding="0" cellspacing="0">
+          <tr>
+           <td>Select Ruleset</td>
+           <td align="right">    
+          <a href="index.php?editor=server&action=30"><img src="images/add.gif" border="0" title="Add a Ruleset"></a> 
+           </td>
+           </tr>        
+         </table>
+      </div>
+
+
+       <table class="table_content2" width="100%">
+<? if (isset($ruleset)):?>
+         <tr>
+          <td align="center" width="5%"><strong>id</strong></td>
+          <td align="center" width="5%"><strong>name</strong></td>
+          <th width="5%"></th>
+         </tr>
+<?$x=0; foreach($ruleset as $ruleset=>$v):?>
+        <tr bgcolor="#<? echo ($x % 2 == 0) ? "AAAAAA" : "BBBBBB";?>">
+          <td align="center" width="5%"><?=$v['ruleset_id']?></td>
+          <td align="center" width="5%"><a href="index.php?editor=server&ruleset_id=<?=$v['ruleset_id']?>&action=28"><?=$v['name']?></td>
+          <td align="right">  
+            <a href="index.php?editor=server&ruleset_id=<?=$v['ruleset_id']?>&action=22"><img src="images/edit2.gif" border="0" title="Edit Ruleset"></a>  
+            <a href="index.php?editor=server&name=<?=$v['name']?>&action=25"><img src="images/last.gif" border="0" title="Copy this ruleset"></a>
+            <a onClick="return confirm('WARNING: This will delete ruleset <?=$v['ruleset_id']?> and all associated rules! Do you wish to continue?');" href="index.php?editor=server&ruleset_id=<?=$v['ruleset_id']?>&action=29"><img src="images/remove3.gif" border="0" title="Delete ruleset and all assoicated rules"></a>
+          </td>
+        </tr>
+        <?$x++; endforeach;?>
+        </table>
+        <?endif;?>
