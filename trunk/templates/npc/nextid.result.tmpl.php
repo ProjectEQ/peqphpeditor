ae08fb6ae935747d93109300630d4829f404903c
cavedude00 <cavedude00@e2836f04-be38-11dd-ab14-3be0eea8eb7e>
2009-10-23 07:15:35 +0000

    Small tool to get the next available NPCID in a zone using PEQ's ID system.

---
+++ trunk/templates/npc/nextid.result.tmpl.php
@@ -0,0 +1,18 @@
+ <div class="table_container" style="width: 200px">
+        <div class="table_header">
+        <table width="100%" cellpadding="0" cellspacing="0">
+          <tr>
+           <td>NPC ID:</td>
+           <td align="right">    
+          <a href="index.php?editor=npc&z=<?=$currzone?>&npcid=<?=$npcid?>">Go Back</a>
+            </td>
+           </tr>        
+         </table>
+      </div>
+      <div class="table_content">
+      <div style="padding: 5px 0px 0px 20px;">
+      <?=$next_npcid?>
+      </div>
+     </div>
+      </div>
+      <br>
