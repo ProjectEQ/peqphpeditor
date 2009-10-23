f00158e52325c06532926f809030a86cb81e9f91
cavedude00 <cavedude00@e2836f04-be38-11dd-ab14-3be0eea8eb7e>
2009-10-23 07:15:35 +0000

    Small tool to get the next available NPCID in a zone using PEQ's ID system.

---
+++ templates/npc/nextid.tmpl.php
@@ -0,0 +1,22 @@
+<div class="edit_form" style="width: 200px">
+      <div class="edit_form_header">
+        Find next available NPCID in
+      </div>
+      <div class="edit_form_content">
+        <form name="npc_id" method="post" action=index.php?editor=npc&z=<?=$currzone?>&npcid=<?=$npcid?>&action=41>
+        <table width="100%">
+          <tr>
+            <td align="left" width="30%"> Zone:<br>
+            <select name="npczoneid" style="width: 180px;">
+<?foreach($zoneids as $key=>$value):?>
+                   <option value="<?=$key?>"<?echo ($key == $zoneid)? " selected" : "";?>><?=$key?>: <?=$value?></option>
+<?endforeach;?>
+            </select>
+          </tr>          
+              </table><br><br>
+        <center>
+          <input type="submit" value="Submit Changes">
+        </center>
+      </form>
+      </div>
+      </div>
