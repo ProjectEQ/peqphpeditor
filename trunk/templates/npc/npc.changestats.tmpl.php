4ffa9e0dac9f7c0493cec002b820097ff20f8d15
cavedude00 <cavedude00@e2836f04-be38-11dd-ab14-3be0eea8eb7e>
2010-06-18 19:53:47 +0000

    Added the ability to change NPC AC, while giving many options in regards to power of the NPC, and which NPCs to change. Any changes will only effect NPCS in the selected zone. This is a precursor to change any NPC stat beyond AC once reasonable formulas are created.

---
+++ trunk/templates/npc/npc.changestats.tmpl.php
@@ -0,0 +1,69 @@
+<div class="edit_form" style="width: 700px">
+      <div class="edit_form_header">
+        Change NPC Stats
+               </div>
+        <div class="edit_form_content">
+        <form method="post" action="index.php?editor=npc&z=<?=$currzone?>&zoneid=<?=$currzoneid?>&npcid=<?=$npcid?>&action=53">
+              <table width="100%">
+           <tr>
+               <td valign="top">
+NPC Type:<br>
+<select name="npctype_selected">
+<?foreach($npctype as $key=>$value):?>
+                   <option value="<?=$key?>"<?echo ($key == $npctype_selected)? " selected" : "";?>><?=$value?></option>
+<?endforeach;?>
+                 </select><br><br>  
+</td> 
+<td valign="top">   
+NPC Tier:<br>       
+<select name="npctier_selected">
+<?foreach($npctier as $key=>$value):?>
+                   <option value="<?=$key?>"<?echo ($key == $npctier_selected)? " selected" : "";?>> <?=$value?></option>
+<?endforeach;?>
+                 </select>               
+            </td>
+<td valign="top">   
+NPC to Change:<br>       
+<select name="npcchange_selected">
+<?foreach($npcchange as $key=>$value):?>
+                   <option value="<?=$key?>"<?echo ($key == $npcchange_selected)? " selected" : "";?>> <?=$value?></option>
+<?endforeach;?>
+                 </select>               
+            </td>
+
+             </tr>
+                 </table>
+<center><B>WHERE<B><br></center>
+<table width="100%">
+           <tr>
+               <td valign="top">
+Name:<br>
+<input type="text" size="25" name="npcname" value=""></td> 
+<td valign="top">   
+Race:<br>       
+<select name="race_selected">
+<?foreach($races as $key=>$value):?>
+                   <option value="<?=$key?>"<?echo ($key == $race_selected)? " selected" : "";?>><?=$key?>: <?=$value?></option>
+<?endforeach;?>
+                 </select>               
+            </td>
+<td valign="top">   
+Class:<br>       
+<select name="class_selected">
+<?foreach($classes as $key=>$value):?>
+                   <option value="<?=$key?>"<?echo ($key == $class_selected)? " selected" : "";?>><?=$key?>: <?=$value?></option>
+<?endforeach;?>
+                 </select>               
+            </td>
+
+             </tr>
+                 </table><br><br>
+
+               <center>
+               
+                 <input type="submit">
+               </center>
+             </form>
+         </div>
+      </div>
+  
