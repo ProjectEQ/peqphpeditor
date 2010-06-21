54904060841db8192d22f252fe7a89c317d7252e
cavedude00 <cavedude00@e2836f04-be38-11dd-ab14-3be0eea8eb7e>
2010-06-21 03:37:38 +0000

    NPCID and ZoneID will be saved when Spells tab is selected, and the spellset option is chosen.

--- templates/spellops/spellops.default.tmpl.php
+++ templates/spellops/spellops.default.tmpl.php
@@ -7,7 +7,7 @@
          <tr>
            <td class="edit_form_content">
              <center>
-               <a href="index.php?editor=spellset">Edit Spell Sets</a><br>
+               <a href="index.php?editor=spellset&z=<?=$currzone?>&zoneid=<?=$currzoneid?>&npcid=<?=$npcid?>">Edit Spell Sets</a><br>
                <a href="index.php?editor=spells">Edit Spells</a><br>
                <a href="index.php?editor=spells&action=10">Generate spells_us.txt</a>
              </center>
