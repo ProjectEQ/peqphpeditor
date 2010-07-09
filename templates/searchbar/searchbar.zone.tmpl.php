4fc3b332f731c525a39db07df7c8724cec6a3c92
jeremiah.oligario <jeremiah.oligario@e2836f04-be38-11dd-ab14-3be0eea8eb7e>
2010-07-09 08:19:37 +0000

    Added better description to searchbar when searching by zones.

--- templates/searchbar/searchbar.zone.tmpl.php
+++ templates/searchbar/searchbar.zone.tmpl.php
@@ -4,25 +4,24 @@
             <td>
               <strong>1.</strong>
               <select OnChange="gotosite(this.options[this.selectedIndex].value)">
-                <option value="">Select a Zone</option>
+                <option value="">Select a Zone Shortname</option>
 <?php foreach ($zonelist as $zone): ?>
 <?php if ($zone['expansion'] <= $expansion_limit): ?>
                 <option value="index.php?editor=<?=$curreditor?>&z=<?=$zone['short_name']?>&zoneid=<?=$zone['id']?>"<?php if ($currzoneid == $zone['id']): ?> selected<?php endif;?>><?=$zone['short_name']?> (<?=$zone['version']?>)</option>
 <?php endif;?>
 <?php endforeach;?>
               </select>
-             </td>
-             <td>or <strong>&nbsp;2.</strong>
-            <select OnChange="gotosite(this.options[this.selectedIndex].value)">
-                <option value="">Select a Zone</option>
+            </td>
+            <td>or <strong>&nbsp;2.</strong>
+              <select OnChange="gotosite(this.options[this.selectedIndex].value)">
+                <option value="">Select a Zone Longname</option>
 <?php foreach ($zonelist2 as $zone): ?>
 <?php if ($zone['expansion'] <= $expansion_limit): ?>
                 <option value="index.php?editor=<?=$curreditor?>&z=<?=$zone['short_name']?>&zoneid=<?=$zone['id']?>"<?php if ($currzoneid == $zone['id']): ?> selected<?php endif;?>><?=$zone['long_name']?> (<?=$zone['version']?>)</option>
 <?php endif;?>
 <?php endforeach;?>
               </select>
-             </td>
-
-           </tr>
+            </td>
+          </tr>
         </table>
       </div>
