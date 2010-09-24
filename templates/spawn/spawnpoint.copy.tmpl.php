a55b78582160c4dde99dc0cff29a43d2f64ed2c8
jeremiah.oligario <jeremiah.oligario@e2836f04-be38-11dd-ab14-3be0eea8eb7e>
2010-09-24 19:58:30 +0000

    Moved a couple more form tags. Swapped variance and pathgrid fields when adding a spawnpoint to match order of the spawnpoint editor and viewer.

--- templates/spawn/spawnpoint.copy.tmpl.php
+++ templates/spawn/spawnpoint.copy.tmpl.php
@@ -1,26 +1,26 @@
   <form name="spawnpoint" method="post" action="index.php?editor=spawn&z=<?=$currzone?>&zoneid=<?=$currzoneid?>&npcid=<?=$npcid?>&action=51">
-         <div class="edit_form" style="width: 200px;">
-        <div class="edit_form_header">
-         Copy Spawnpoint ID: <?=$id?>
-        </div>
-         <div class="edit_form_content">
-	<center>	          
-     <td align="left" width="17%">Spawngroup ID:<input type="text" name="sgid" size="7" value=""></td><br><br>
-      <input type="submit" value="Copy Spawn">		
-      <input type="hidden" name="id" value="<?=$id?>">  
-      <input type="hidden" name="zone" value="<?=$zone?>">
-      <input type="hidden" name="x" value="<?=$x?>">
-      <input type="hidden" name="y" value="<?=$y?>">
-      <input type="hidden" name="z" value="<?=$z?>">
-      <input type="hidden" name="heading" value="<?=$heading?>">
-      <input type="hidden" name="respawntime" value="<?=$respawntime?>">
-      <input type="hidden" name="variance" value="<?=$variance?>">
-      <input type="hidden" name="pathgrid" value="<?=$pathgrid?>">
-      <input type="hidden" name="condition" value="<?=$_condition?>">
-      <input type="hidden" name="cond_value" value="<?=$cond_value?>">
-      <input type="hidden" name="version" value="<?=$version?>">
-      <input type="hidden" name="enabled" value="<?=$enabled?>">
+    <div class="edit_form" style="width: 200px;">
+      <div class="edit_form_header">
+        Copy Spawnpoint ID: <?=$id?>
+      </div>
+      <div class="edit_form_content">
+	    <center>	          
+          <td align="left" width="17%">Spawngroup ID:<input type="text" name="sgid" size="7" value=""></td><br><br>
+            <input type="submit" value="Copy Spawn">		
+            <input type="hidden" name="id" value="<?=$id?>">  
+            <input type="hidden" name="zone" value="<?=$zone?>">
+            <input type="hidden" name="x" value="<?=$x?>">
+            <input type="hidden" name="y" value="<?=$y?>">
+            <input type="hidden" name="z" value="<?=$z?>">
+            <input type="hidden" name="heading" value="<?=$heading?>">
+            <input type="hidden" name="respawntime" value="<?=$respawntime?>">
+            <input type="hidden" name="variance" value="<?=$variance?>">
+            <input type="hidden" name="pathgrid" value="<?=$pathgrid?>">
+            <input type="hidden" name="condition" value="<?=$_condition?>">
+            <input type="hidden" name="cond_value" value="<?=$cond_value?>">
+            <input type="hidden" name="version" value="<?=$version?>">
+            <input type="hidden" name="enabled" value="<?=$enabled?>">
         </center>
+      </div>
+    </div>
   </form>
-      </div>
-      </div>
--- templates/spawn/spawnpoint.move.tmpl.php
+++ templates/spawn/spawnpoint.move.tmpl.php
@@ -1,14 +1,14 @@
   <form name="spawnpoint" method="post" action="index.php?editor=spawn&z=<?=$currzone?>&zoneid=<?=$currzoneid?>&npcid=<?=$npcid?>&action=53">
-         <div class="edit_form" style="width: 200px;">
-        <div class="edit_form_header">
-         Move Spawnpoint ID: <?=$id?>
-        </div>
-         <div class="edit_form_content">
-	<center>	          
-     <td align="left" width="17%">Spawngroup ID:<input type="text" name="sgid" size="7" value=""></td><br><br>
-        <input type="submit" value="Move Spawn">
-        <input type="hidden" name="id" value="<?=$id?>">
+    <div class="edit_form" style="width: 200px;">
+      <div class="edit_form_header">
+        Move Spawnpoint ID: <?=$id?>
+      </div>
+      <div class="edit_form_content">
+	    <center>	          
+          <td align="left" width="17%">Spawngroup ID:<input type="text" name="sgid" size="7" value=""></td><br><br>
+          <input type="submit" value="Move Spawn">
+          <input type="hidden" name="id" value="<?=$id?>">
         </center>
+      </div>
+      </div>
   </form>
-      </div>
-      </div>
