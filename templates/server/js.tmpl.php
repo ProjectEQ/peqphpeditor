e2ba88aee2916fffae96a8e7b79fed3e2427a925
jeremiah.oligario <jeremiah.oligario@e2836f04-be38-11dd-ab14-3be0eea8eb7e>
2010-12-04 16:24:59 +0000

    Disabled optional note block if notification is not sent in bug status update.

--- templates/server/js.tmpl.php
+++ templates/server/js.tmpl.php
@@ -2,4 +2,12 @@
     function toggleNotify() {
       document.getElementById("notify").style.display = "inline";
     }
+    function toggleNote() {
+      if (document.getElementById("note").disabled == true) {
+        document.getElementById("note").disabled = false;
+      }
+      else {
+        document.getElementById("note").disabled = true;
+      }
+    }
   </script>
