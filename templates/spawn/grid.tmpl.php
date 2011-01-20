ecfb60cae0d7bd8c1b9a3ee7af6f48a509db45aa
jeremiah.oligario <jeremiah.oligario@e2836f04-be38-11dd-ab14-3be0eea8eb7e>
2011-01-20 23:26:14 +0000

    Added SQL export ability to grids. Added ability to copy grid entries.

--- templates/spawn/grid.tmpl.php
+++ templates/spawn/grid.tmpl.php
@@ -1,34 +1,42 @@
-     <div class="table_container" style="width: 200px">
-        <div class="table_header">
-        <div style="float: right">
-         <a onClick="return confirm('Really Delete Grid <?=$pathgrid?>?');" href="index.php?editor=spawn&z=<?=$currzone?>&zoneid=<?=$currzoneid?>&npcid=<?=$npcid?>&spid=<?=$spid?>&pathgrid=<?=$pathgrid?>&number=<?=$number?>&action=29"><img src="images/remove3.gif" border="0" title="Delete Grid"></a>
-        </div>
-        Grid: <?=$pathgrid?>
+  <?$export_sql = export_grid_sql();?>
+  <div id="sql_block" style="display:none">
+    <center>
+      <textarea id="sql_text" rows="3" cols="90"><?=$export_sql?></textarea><br><br>
+      <button type="button" id="copy_sql" onClick="clipboardData.setData('Text', sql_text.value);">Copy SQL to Clipboard</button>&nbsp;
+      <button type="button" id="hide_sql" onClick="document.getElementById('sql_block').style.display='none';">Hide SQL</button>
+    </center>
+    <br/>
+  </div>
+  <div class="table_container" style="width: 200px">
+    <div class="table_header">
+      <div style="float: right">
+        <a onClick="document.getElementById('sql_block').style.display='block';"><img src="images/sql.gif" border=0 title="Show SQL"></a>
+        <a onClick="return confirm('Really Delete Grid <?=$pathgrid?>?');" href="index.php?editor=spawn&z=<?=$currzone?>&zoneid=<?=$currzoneid?>&npcid=<?=$npcid?>&spid=<?=$spid?>&pathgrid=<?=$pathgrid?>&number=<?=$number?>&action=29"><img src="images/remove3.gif" border="0" title="Delete Grid"></a>
       </div>
-      <div class="table_content">
-        Grid [<a href="index.php?editor=spawn&z=<?=$currzone?>&zoneid=<?=$currzoneid?>&npcid=<?=$npcid?>&spid=<?=$spid?>&pathgrid=<?=$pathgrid?>&action=21">edit</a>]:<br>
+      Grid: <?=$pathgrid?>
+    </div>
+    <div class="table_content">
+      Grid [<a href="index.php?editor=spawn&z=<?=$currzone?>&zoneid=<?=$currzoneid?>&npcid=<?=$npcid?>&spid=<?=$spid?>&pathgrid=<?=$pathgrid?>&action=21">edit</a>]:<br>
       <div style="padding: 5px 0px 0px 20px;">
         Wander Type: <?=$wandertype[$type]?><br>
         Pause Type: <?=$pausetype[$type2]?><br>
       </div>
-        </div>
-      </div>
-      <br>
-
-      <div class="table_container" style="width: 500px">
-      <div class="table_header">
-        <table width="100%" cellpadding="0" cellspacing="0">
-          <tr>
-           <td>Grid Entries:</td>
-           <td align="right">    
-          <a href="index.php?editor=spawn&z=<?=$currzone?>&zoneid=<?=$currzoneid?>&npcid=<?=$npcid?>&spid=<?=$spid?>&pathgrid=<?=$pathgrid?>&action=27"><img src="images/add.gif" border="0" title="Add an item to this Grid Entry Table"></a>
-          <a onClick="return confirm('Really delete these Grid Entries from Grid <?=$pathgrid?>?');" href="index.php?editor=spawn&z=<?=$currzone?>&zoneid=<?=$currzoneid?>&npcid=<?=$npcid?>&spid=<?=$spid?>&pathgrid=<?=$pathgrid?>&number=<?=$number?>&action=26"><img src="images/table.gif" border="0" title="Permanently delete this Grid Entry set"></a>
-            </td>
-           </tr>        
-         </table>
-      </div>
- 
-      <table class="table_content2" width="100%">
+    </div>
+  </div>
+  <br>
+  <div class="table_container" style="width: 500px">
+    <div class="table_header">
+      <table width="100%" cellpadding="0" cellspacing="0">
+        <tr>
+          <td>Grid Entries:</td>
+          <td align="right">    
+            <a href="index.php?editor=spawn&z=<?=$currzone?>&zoneid=<?=$currzoneid?>&npcid=<?=$npcid?>&spid=<?=$spid?>&pathgrid=<?=$pathgrid?>&action=27"><img src="images/add.gif" border="0" title="Add an item to this Grid Entry Table"></a>
+            <a onClick="return confirm('Really delete these Grid Entries from Grid <?=$pathgrid?>?');" href="index.php?editor=spawn&z=<?=$currzone?>&zoneid=<?=$currzoneid?>&npcid=<?=$npcid?>&spid=<?=$spid?>&pathgrid=<?=$pathgrid?>&number=<?=$number?>&action=26"><img src="images/table.gif" border="0" title="Permanently delete this Grid Entry set"></a>
+          </td>
+        </tr>        
+      </table>
+    </div>
+    <table class="table_content2" width="100%">
 <? if (isset($grids)):?>
          <tr>
           <td align="center" width="5%"><strong>Number</strong></td>
@@ -37,10 +45,10 @@
           <td align="center" width="10%"><strong>Z</strong></td>
           <td align="center" width="10%"><strong>Heading</strong></td>
           <td align="center" width="10%"><strong>Pause</strong></td>
-          <th width="5%"></th>
+          <th width="8%"></th>
          </tr>
 <?$x=0; foreach($grids as $number=>$v):?>
-        <tr bgcolor="#<? echo ($x % 2 == 0) ? "AAAAAA" : "BBBBBB";?>">
+        <tr bgcolor="#<? echo ($x % 2 == 0) ? "BBBBBB" : "AAAAAA";?>">
           <td align="center" width="5%"><?=$number?></td>
           <td align="center" width="10%"><?=$v['x_coord']?></td>
           <td align="center" width="10%"><?=$v['y_coord']?></td>
@@ -49,6 +57,8 @@
           <td align="center" width="10%"><?=$v['pause']?></td>
           <td align="right">      
             <a href="index.php?editor=spawn&z=<?=$currzone?>&zoneid=<?=$currzoneid?>&npcid=<?=$npcid?>&spid=<?=$spid?>&pathgrid=<?=$pathgrid?>&number=<?=$number?>&action=24"><img src="images/edit2.gif" border="0" title="Edit Grid Entry"></a>
+<?$copy_string = "&x_coord=" . $v['x_coord'] . "&y_coord=" . $v['y_coord'] . "&z_coord=" . $v['z_coord'] . "&heading=" . $v['heading'] . "&pause=" . $v['pause'];?>
+            <a href="index.php?editor=spawn&z=<?=$currzone?>&zoneid=<?=$currzoneid?>&npcid=<?=$npcid?>&spid=<?=$spid?>&pathgrid=<?=$pathgrid?>&number=<?=$number?>&action=63<?echo $copy_string?>"><img src="images/movefile.gif" border="0" title="Copy Grid Entry"></a>
             <a href="index.php?editor=spawn&z=<?=$currzone?>&zoneid=<?=$currzoneid?>&npcid=<?=$npcid?>&spid=<?=$spid?>&pathgrid=<?=$pathgrid?>&number=<?=$number?>&action=23"><img src="images/remove3.gif" border="0" title="Remove Grid Entry"></a>
           </td>
         </tr>
--- templates/spawn/gridentry.add.tmpl.php
+++ templates/spawn/gridentry.add.tmpl.php
@@ -1,10 +1,9 @@
-      <div class="edit_form" style="width: 500px">
-      <div class="edit_form_header">
-        Add a Grid Entry <?=$pathgrid?>       
-     </div>
-
-      <div class="edit_form_content">
-        <form name="gridentry" method="post" action="index.php?editor=spawn&z=<?=$currzone?>&zoneid=<?=$currzoneid?>&npcid=<?=$npcid?>&spid=<?=$spid?>&pathgrid=<?$pathgrid?>&action=28">
+  <div class="edit_form" style="width: 500px">
+    <div class="edit_form_header">
+      Add a Grid Entry <?=$pathgrid?>       
+    </div>
+    <div class="edit_form_content">
+      <form name="gridentry" method="post" action="index.php?editor=spawn&z=<?=$currzone?>&zoneid=<?=$currzoneid?>&npcid=<?=$npcid?>&spid=<?=$spid?>&pathgrid=<?$pathgrid?>&action=28">
         <table width="100%">
           <tr>
             <th>Number:</th>
@@ -16,19 +15,18 @@
           </tr>
           <tr>
             <td><input type="text" size="7" name="number" value="<?=$suggestednum?>"></td>
-            <td><input type="text" size="7" name="x_coord" value="0"></td>
-            <td><input type="text" size="7" name="y_coord" value="0"></td>
-            <td><input type="text" size="7" name="z_coord" value="0"></td>
-            <td><input type="text" size="7" name="heading" value="-1"></td>
-            <td><input type="text" size="7" name="pause" value="0"></td>
+            <td><input type="text" size="7" name="x_coord" value="<?echo ($x_coord) ? $x_coord : 0;?>"></td>
+            <td><input type="text" size="7" name="y_coord" value="<?echo ($y_coord) ? $y_coord : 0;?>"></td>
+            <td><input type="text" size="7" name="z_coord" value="<?echo ($z_coord) ? $z_coord : 0;?>"></td>
+            <td><input type="text" size="7" name="heading" value="<?echo ($heading) ? $heading : -1;?>"></td>
+            <td><input type="text" size="7" name="pause" value="<?echo ($pause) ? $pause : 0;?>"></td>
           </tr>
         </table><br><br>
-
         <center>
           <input type="hidden" name="pathgrid" value="<?=$pathgrid?>">
           <input type="hidden" name="zoneid" value="<?=$zid?>">
           <input type="submit" value="Submit Changes">
         </center>
       </form>
-      </div>
-      </div>
+    </div>
+  </div>
