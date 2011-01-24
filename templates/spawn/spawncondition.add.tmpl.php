c35b1eee28b09f11ad1e1611bb829a823a9ba916
jeremiah.oligario <jeremiah.oligario@e2836f04-be38-11dd-ab14-3be0eea8eb7e>
2011-01-24 20:26:48 +0000

    Added missing open-quotes in spawn condition add/edit.

--- templates/spawn/spawncondition.add.tmpl.php
+++ templates/spawn/spawncondition.add.tmpl.php
@@ -4,7 +4,7 @@
       </div>
 
       <div class="edit_form_content">
-        <form name="spawncondition" method="post" action=index.php?editor=spawn&z=<?=$currzone?>&zoneid=<?=$currzoneid?>&npcid=<?=$npcid?>&spid=<?=$spid?>&action=46">
+        <form name="spawncondition" method="post" action="index.php?editor=spawn&z=<?=$currzone?>&zoneid=<?=$currzoneid?>&npcid=<?=$npcid?>&spid=<?=$spid?>&action=46">
         <table width="100%">
           <tr>
             <th>ID:</th>
