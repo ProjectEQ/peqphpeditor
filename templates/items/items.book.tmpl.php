d2a7d4de2b257979eae21fef05b8a5140378d684
jeremiah.oligario <jeremiah.oligario@e2836f04-be38-11dd-ab14-3be0eea8eb7e>
2011-06-15 13:33:14 +0000

    Will return to previous page when deleting faction. Book page now available for charms. Touched up item edit page.

--- templates/items/items.book.tmpl.php
+++ templates/items/items.book.tmpl.php
@@ -1,24 +1,22 @@
-     <div class="table_container" style="width: 750px;">
-      <div class="table_header">
-        <table width="100%" cellpadding="0" cellspacing="0">
-          <tr>
-           <td>Book text for <?=$name?></td>
-            </tr>        
-         </table>
-      </div>
-
-       <div class="edit_form_content">
-        <form name="booktext" method="post" action=index.php?editor=items&id=<?=$id?>&name=<?=$name?>&action=4">
+  <div class="table_container" style="width: 750px;">
+    <div class="table_header">
+      <table width="100%" cellpadding="0" cellspacing="0">
+        <tr>
+          <td>Book text for <?=$name?>:</td>
+        </tr>
+      </table>
+    </div>
+    <div class="edit_form_content">
+      <form name="booktext" method="post" action="index.php?editor=items&id=<?=$id?>&name=<?=$name?>&action=4">
         <table width="100%">
-          <td><textarea name="txtfile" rows=20 cols=88><?=$txtfile?></textarea></td>
-                   <td align="right">             
-          </td>
-        </table><br>
+          <td><textarea name="txtfile" rows="20" cols="88"><?=$txtfile?></textarea></td>
+          <td align="right">&nbsp;</td>
+        </table><br/>
         <center>
           <input type="hidden" name="name" value="<?=$name?>">
           <input type="hidden" name="id" value="<?=$id?>">
           <input type="submit" value="Submit Changes">
         </center>
       </form>
-      </div>
-      </div>
+    </div>
+  </div>
