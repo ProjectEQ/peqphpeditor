3a0f8e2d718dd8ab1f154a8cd8d4cc1507fbd55e
jeremiah.oligario <jeremiah.oligario@e2836f04-be38-11dd-ab14-3be0eea8eb7e>
2011-09-24 14:31:01 +0000

    A little more verbose on hackers view.

--- templates/server/hackers.tmpl.php
+++ templates/server/hackers.tmpl.php
@@ -11,19 +11,21 @@
 <?if (isset($hackers)) :?>
             <tr>
               <td align="center" width="5%"><strong>ID</strong></td>
-              <td align="center" width="5%"><strong>Account</strong></td>
-              <td align="center" width="5%"><strong>Name</strong></td>
-              <td align="center" width="5%"><strong>Zone</strong></td>
-              <td align="center" width="15%"><strong>Date</strong></td>
+              <td align="center" width="10%"><strong>Account</strong></td>
+              <td align="center" width="15%"><strong>Name</strong></td>
+              <td align="center" width="15%"><strong>Zone</strong></td>
+              <td align="center" width="20%"><strong>Date</strong></td>
+              <td align="center" width="30%"><strong>Hack</strong></td>
               <td width="5%">&nbsp;</td>
             </tr>
 <?$x=0; foreach($hackers as $hackers=>$v):?>
             <tr bgcolor="#<? echo ($x % 2 == 0) ? "BBBBBB" : "AAAAAA";?>">
               <td align="center" width="5%"><?=$v['hid']?></td>
-              <td align="center" width="5%"><?=$v['account']?></td>
-              <td align="center" width="5%"><?=$v['name']?></td>
-              <td align="center" width="5%"><?=$v['zone']?></td>
-              <td align="center" width="15%"><?=$v['date']?></td>
+              <td align="center" width="10%"><?=$v['account']?></td>
+              <td align="center" width="15%"><?=$v['name']?></td>
+              <td align="center" width="15%"><?echo ($v['zone']) ? $v['zone'] : "N/A";?></td>
+              <td align="center" width="20%"><?=$v['date']?></td>
+              <td align="center" width="30%"><?=substr($v['hacked'],0,23)?></td>
               <td align="right"><a href="index.php?editor=server&hid=<?=$v['hid']?>&action=8"><img src="images/edit2.gif" border="0" title="View Hacker"></a>&nbsp;<a onClick="return confirm('Really Delete Entry <?=$v['hid']?>?');" href="index.php?editor=server&hid=<?=$v['hid']?>&action=7"><img src="images/remove3.gif" border="0" title="Delete this entry"></a></td>
             </tr>
 <?$x++; endforeach;?>
