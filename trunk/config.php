9f9e53c8ec9fa06836e26480ca1ddeaf90d80bbc
cavedude00@gmail.com <cavedude00@gmail.com@e2836f04-be38-11dd-ab14-3be0eea8eb7e>
2012-04-27 16:13:10 +0000

    We'll need to be able to edit EmoteID

--- trunk/config.php
+++ trunk/config.php
@@ -2,9 +2,9 @@
 
 // Database Information:
 $dbhost = 'localhost';
-$dbuser = 'username';
-$dbpass = 'password';
-$db = 'database_name';
+$dbuser = 'eqemu';
+$dbpass = 'eqemu';
+$db = 'eqemu';
 
 /*  Limit zone lists to a specified expansion
  *  (i.e. setting $expansion_limit to 2 would cause only Classic and Kunark zones
@@ -35,11 +35,14 @@ $expansion_limit = 19;
 // How NPCs are listed. 1 = by NPCID (zoneidnumber*1000), 2 = By spawn2 entry
 $npc_list = 1;
 
+// Spawngroup list limit. Limits how many spawngroups are displayed as result of a Coord/NPC search. Specific NPC lists are not effected.
+$spawngroup_limit = 150;
+
 // Dont want to have to type the username and password every time you start the editor?
 // Set the two variables below to the values you want to be in the form when you start it up.
 // (default login: admin  pw: password)
-$login = 'admin';
-$password = 'password';
+//$login = 'admin';
+//$password = 'password';
 
 // Log SQL queries:  1 = on, 0 = off
 $logging = 1;
@@ -51,5 +54,20 @@ $logging = 1;
 // Automatically create new logs monthly.
 $filetime = date("m-Y");
 $log_file = "logs/sql_log_$filetime.sql";
+
+// Log all MySQL queries (If disabled only write entries are logged - recommended.)
+$log_all = 0;
+
+// Log all MySQL queries that result in an error.
+$log_error = 1;
+
+// Enable or disable user logins.
+$enable_user_login = 1;
+
+// Enable or disable read only guest mode log in.
+$enable_guest_mode = 1;
+
+// Path for quests without trailing slash.
+$quest_path = "/home/administrator/run/quests"
 
 ?>
