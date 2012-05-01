780dba039d66420f8d53d2c4d2261b0a0020b21c
jeremiah.oligario <jeremiah.oligario@e2836f04-be38-11dd-ab14-3be0eea8eb7e>
2012-05-01 16:27:03 +0000

    Minor HTML tag fixes. Reverted config change to defaults.

--- templates/npc/emotes.add.tmpl.php
+++ templates/npc/emotes.add.tmpl.php
@@ -1,29 +1,29 @@
-      <form method="post" action="index.php?editor=npc&z=<?=$currzone?>&zoneid=<?=$currzoneid?>&npcid=<?=$npcid?>&action=77">
-      <div class="edit_form" style="width: 510px;">
-        <div class="edit_form_header">
-          Add emote to set <?=$emoteid?> Current NPC is: <?=$npcid?>
-        </div>
-        <div class="edit_form_content">
-                        <center>
-            <strong>EmoteID</strong><br>
-            <input class="indented" id="id" type="text" name="emoteid" size="7" value="<?=$emoteid?>"><br><br>
-            <strong>Event</strong><br>
-            <select name="event_" style="width: 150px;">
+  <form method="post" action="index.php?editor=npc&z=<?=$currzone?>&zoneid=<?=$currzoneid?>&npcid=<?=$npcid?>&action=77">
+    <div class="edit_form" style="width: 510px;">
+      <div class="edit_form_header">
+        Add emote to set <?=$emoteid?> Current NPC is: <?=$npcid?>
+      </div>
+      <div class="edit_form_content">
+        <center>
+          <strong>EmoteID</strong><br>
+          <input class="indented" id="id" type="text" name="emoteid" size="7" value="<?=$emoteid?>"><br><br>
+          <strong>Event</strong><br>
+          <select name="event_" style="width: 150px;">
 <?foreach($eventtype as $key=>$value):?>
-                   <option value="<?=$key?>"<?echo ($key == $event_)? " selected" : "";?>><?=$key?>: <?=$value?></option>
+            <option value="<?=$key?>"<?echo ($key == $event_)? " selected" : "";?>><?=$key?>: <?=$value?></option>
 <?endforeach;?>
-                 </select><br><br>
-            <strong>Type</strong><br>
-            <select name="type" style="width: 100px;">
+          </select><br><br>
+          <strong>Type</strong><br>
+          <select name="type" style="width: 100px;">
 <?foreach($emotetype as $key=>$value):?>
-                   <option value="<?=$key?>"<?echo ($key == $type)? " selected" : "";?>><?=$key?>: <?=$value?></option>
+            <option value="<?=$key?>"<?echo ($key == $type)? " selected" : "";?>><?=$key?>: <?=$value?></option>
 <?endforeach;?>
-                 </select><br><br>
-            <strong>Emote</strong><br>
-            <input class="indented" id="id" type="text" name="text" size="75" value=""><br><br>
+          </select><br><br>
+          <strong>Emote</strong><br>
+          <input class="indented" id="id" type="text" name="text" size="75" value=""><br><br>
           <input type="hidden" name="emoteid" value="<?=$emoteid?>">
           <input type="submit" value="Submit Changes">
         </center>
-      </form>
       </div>
-      </div>
+    </div>
+  </form>
--- templates/npc/emotes.tmpl.php
+++ templates/npc/emotes.tmpl.php
@@ -1,44 +1,44 @@
-      <div class="table_container" style="width: 700px;">
-      <div class="table_header">
-        <table width="100%" cellpadding="0" cellspacing="0">
-          <tr>
-           <td>NPC Emotes</td>
-           <td align="right">  
-          <?if($emoteid != $npcid) {?>
-          <a href="index.php?editor=npc&z=<?=$currzone?>&zoneid=<?=$currzoneid?>&npcid=<?=$npcid?>&emoteid=<?=$emoteid?>&action=76"><img src="images/add.gif" border="0" title="Add an entry to this emote set"></a>
-          <?} else {?>
-          <a href="index.php?editor=npc&z=<?=$currzone?>&zoneid=<?=$currzoneid?>&npcid=<?=$npcid?>&action=78"><img src="images/add.gif" border="0" title="Add an entry to this emote set"></a>
-          <?}?>
-            </td>
-           </tr>        
-         </table>
-      </div>
-
-       <table class="table_content2" width="100%">
-<? if (isset($emotes)):?>
-         <tr>
-          <td align="center" width="10%"><strong>EmoteID</strong></td>
-          <td align="center" width="10%"><strong>Event</strong></td>
-          <td align="center" width="10%"><strong>Type</strong></td>
-          <td align="center" width="65%"><strong>Text</strong></td>
-          <th width="5%"></th>
-         </tr>
-<?$x=0; foreach($emotes as $emotes=>$v):?>
-        <tr bgcolor="#<? echo ($x % 2 == 0) ? "BBBBBB" : "AAAAAA";?>">
-          <td align="center" width="10%"><?=$v['emoteid']?></td>
-          <td align="center" width="10%"><?=$eventtype[$v['event_']]?></td>
-          <td align="center" width="10%"><?=$emotetype[$v['type']]?></td>
-          <td align="center" width="65%"><?=$v['text']?></td>
-          <td align="right">      
-            <a href="index.php?editor=npc&z=<?=$currzone?>&zoneid=<?=$currzoneid?>&npcid=<?=$npcid?>&id=<?=$v['id']?>&action=74"><img src="images/edit2.gif" border="0" title="Edit Entry"></a>          
-            <a onClick="return confirm('Really Delete Entry <?=$v['id']?>?');" href="index.php?editor=npc&z=<?=$currzone?>&zoneid=<?=$currzoneid?>&npcid=<?=$npcid?>&emoteid=<?=$v['emoteid']?>&id=<?=$v['id']?>&action=73"><img src="images/remove3.gif" border="0" title="Delete this entry"></a>
-          </td>
-        </tr>
-        <?$x++; endforeach;?>
-        </table>
-        <?endif;?>
-<? if (!isset($emotes)):?>
+  <div class="table_container" style="width: 700px;">
+    <div class="table_header">
+      <table width="100%" cellpadding="0" cellspacing="0">
         <tr>
-          <td align="left" width="100" style="padding: 10px;">No NPC Emotes</td>
-        </tr>
+          <td>NPC Emotes</td>
+          <td align="right">  
+<?if($emoteid != $npcid) {?>
+            <a href="index.php?editor=npc&z=<?=$currzone?>&zoneid=<?=$currzoneid?>&npcid=<?=$npcid?>&emoteid=<?=$emoteid?>&action=76"><img src="images/add.gif" border="0" title="Add an entry to this emote set"></a>
+<?} else {?>
+            <a href="index.php?editor=npc&z=<?=$currzone?>&zoneid=<?=$currzoneid?>&npcid=<?=$npcid?>&action=78"><img src="images/add.gif" border="0" title="Add an entry to this emote set"></a>
+<?}?>
+          </td>
+        </tr>        
+      </table>
+    </div>
+    <table class="table_content2" width="100%">
+<? if (isset($emotes)):?>
+      <tr>
+        <td align="center" width="10%"><strong>EmoteID</strong></td>
+        <td align="center" width="10%"><strong>Event</strong></td>
+        <td align="center" width="10%"><strong>Type</strong></td>
+        <td align="center" width="65%"><strong>Text</strong></td>
+        <th width="5%"></th>
+      </tr>
+<?$x=0; foreach($emotes as $emotes=>$v):?>
+      <tr bgcolor="#<? echo ($x % 2 == 0) ? "BBBBBB" : "AAAAAA";?>">
+        <td align="center" width="10%"><?=$v['emoteid']?></td>
+        <td align="center" width="10%"><?=$eventtype[$v['event_']]?></td>
+        <td align="center" width="10%"><?=$emotetype[$v['type']]?></td>
+        <td align="center" width="65%"><?=$v['text']?></td>
+        <td align="right">      
+          <a href="index.php?editor=npc&z=<?=$currzone?>&zoneid=<?=$currzoneid?>&npcid=<?=$npcid?>&id=<?=$v['id']?>&action=74"><img src="images/edit2.gif" border="0" title="Edit Entry"></a>          
+          <a onClick="return confirm('Really Delete Entry <?=$v['id']?>?');" href="index.php?editor=npc&z=<?=$currzone?>&zoneid=<?=$currzoneid?>&npcid=<?=$npcid?>&emoteid=<?=$v['emoteid']?>&id=<?=$v['id']?>&action=73"><img src="images/remove3.gif" border="0" title="Delete this entry"></a>
+        </td>
+      </tr>
+<?$x++; endforeach;?>
 <?endif;?>
+<? if (!isset($emotes)):?>
+      <tr>
+        <td align="left" width="100" style="padding: 10px;">No NPC Emotes</td>
+      </tr>
+<?endif;?>
+    </table>
+  </div>
