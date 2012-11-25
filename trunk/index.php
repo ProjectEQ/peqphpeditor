dfdd1cd4d043d0736fdb908a90836a68729a6f76
jeremiah.oligario <jeremiah.oligario@e2836f04-be38-11dd-ab14-3be0eea8eb7e>
2012-11-25 16:04:47 +0000

    Added ability for admin to edit account status.

--- trunk/index.php
+++ trunk/index.php
@@ -1,6 +1,6 @@
 <?php
 
-$current_revision = "r344";
+$current_revision = "r349";
 
 require_once("config.php");
 require_once("lib/logging.php");
---
+++ trunk/templates/account/account.status.tmpl.php
@@ -0,0 +1,14 @@
+  <table class="edit_form">
+    <tr>
+      <td class="edit_form_header">Edit Account Status</td>
+    </tr>
+    <tr>
+      <td class="edit_form_content">
+        <form name="account_status" method="post" action="index.php?editor=account&acctid=<?=$acctid?>&action=8">
+          Account Status: <br/>
+          <input type="text" name="new_acct_status" value="<?=$cur_acct_status?>"><br><br>
+          <center><input type="submit" value="Submit"></center>
+        </form>
+      </td>
+    </tr>
+  </table>
