6773e1b03d17115766505e5a891678135aa81ed8
cavedude00@gmail.com <cavedude00@gmail.com@e2836f04-be38-11dd-ab14-3be0eea8eb7e>
2013-07-29 16:14:51 +0000

    Fixed adding lootdrop from search.

--- trunk/templates/loot/lootdrop.changebysearch.tmpl.php
+++ trunk/templates/loot/lootdrop.changebysearch.tmpl.php
@@ -11,9 +11,13 @@
                Lootdrop ID:<br>
                <input type="text" name="ldid" value="<?=$ldid?>"><br><br>
                Probability:<br>
-               <input type="text" name="prob"><br><br>
+               <input type="text" name="probability" value="100"><br><br>
                Multiplier:<br>
-               <input type="text" name="mult"><br><br>
+               <input type="text" name="multiplier" value=1><br><br>
+		 Droplimit:<br>
+               <input type="text" name="droplimit" value=0><br><br>
+               Mindrop:<br>
+               <input type="text" name="mindrop" value=0><br><br>
                <center>
                  <input type="submit">
                </center>
