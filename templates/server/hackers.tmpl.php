b21f365993c66618dce3984ca24bdb812a51e651
jeremiah.oligario <jeremiah.oligario@e2836f04-be38-11dd-ab14-3be0eea8eb7e>
2013-10-15 02:21:56 +0000

    Add ability to delete multiple hacks at one time.

--- templates/server/hackers.tmpl.php
+++ templates/server/hackers.tmpl.php
@@ -1,4 +1,4 @@
-     <div class="edit_form" id="filter_box" style="width: 700px; display: <?echo ($filter['status'] == 'on') ? 'block' : 'none'?>">
+    <div class="edit_form" id="filter_box" style="width: 700px; display: <?echo ($filter['status'] == 'on') ? 'block' : 'none'?>">
       <div class="edit_form_header">
         <table width="100%" cellpadding="0" cellspacing="0">
           <tr>
@@ -38,52 +38,61 @@
           </table>
         </form>
       </div>
-    </div><br/> 
-       <div class="table_container" style="width: 700px;">
-          <div class="table_header">
-            <table width="100%" cellpadding="0" cellspacing="0">
-              <tr>
-                <td align="left" width="33%">Hackers</td>
-                <td align="center" width="33%">
-                  <?echo ($page > 1) ? "<a href='index.php?editor=server&action=6&page=1" . (($sort != "") ? "&sort=" . $sort : "") . (($filter['status'] == "on") ? $filter['url'] : "") . "'><img src='images/first.gif' border='0' width='12' height='12' title='First'/></a>" : "<img src='images/first.gif' border='0' width='12' height='12'/>";?>
-                  <?echo ($page > 1) ? "<a href='index.php?editor=server&action=6&page=" . ($page - 1) . (($sort != "")  ? "&sort=" . $sort : "") . (($filter['status'] == "on") ? $filter['url'] : "") . "'><img src='images/prev.gif' border='0' width='12' height='12' title='Previous'/></a>" : "<img src='images/prev.gif' border='0' width='12' height='12'/>";?>
-                  <?echo $page . " of " . $pages;?>
-                  <?echo ($page < $pages) ? "<a href='index.php?editor=server&action=6&page=" . ($page + 1) . (($sort != "") ? "&sort=" . $sort : "")  . (($filter['status'] == "on") ? $filter['url'] : "") . "'><img src='images/next.gif' border='0' width='12' height='12' title='Next'/></a>" : "<img src='images/next.gif' border='0' width='12' height='12'/>";?>
-                  <?echo ($page < $pages) ? "<a href='index.php?editor=server&action=6&page=" . $pages . (($sort != "") ? "&sort=" . $sort : "")  . (($filter['status'] == "on") ? $filter['url'] : "") . "'><img src='images/last.gif' border='0' width='12' height='12' title='Last'/></a>" : "<img src='images/last.gif' border='0' width='12' height='12'/>";?>
-                </td>
-                <td align="right" width="33%">
-              <a onClick="document.getElementById('filter_box').style.display='block';"><img src="images/filter.jpg" border="0" height="13" width="13" title="Show filter"></a>&nbsp;
+    </div><br/>
+    <div id="action_buttons_top" style="display:none;">
+      <center><input type="button" value="Delete Marked" onClick="pushIdArray();">&nbsp;<input type="button" value="Cancel" onClick="location.reload();"></center><br/>
+    </div>
+    <div class="table_container" style="width: 750px;">
+      <div class="table_header">
+        <table width="100%" cellpadding="0" cellspacing="0">
+          <tr>
+            <td align="left" width="33%">Hackers</td>
+            <td align="center" width="33%">
+              <?echo ($page > 1) ? "<a href='index.php?editor=server&action=6&page=1" . (($sort != "") ? "&sort=" . $sort : "") . (($filter['status'] == "on") ? $filter['url'] : "") . "'><img src='images/first.gif' border='0' width='12' height='12' title='First'/></a>" : "<img src='images/first.gif' border='0' width='12' height='12'/>";?>
+              <?echo ($page > 1) ? "<a href='index.php?editor=server&action=6&page=" . ($page - 1) . (($sort != "")  ? "&sort=" . $sort : "") . (($filter['status'] == "on") ? $filter['url'] : "") . "'><img src='images/prev.gif' border='0' width='12' height='12' title='Previous'/></a>" : "<img src='images/prev.gif' border='0' width='12' height='12'/>";?>
+              <?echo $page . " of " . $pages;?>
+              <?echo ($page < $pages) ? "<a href='index.php?editor=server&action=6&page=" . ($page + 1) . (($sort != "") ? "&sort=" . $sort : "")  . (($filter['status'] == "on") ? $filter['url'] : "") . "'><img src='images/next.gif' border='0' width='12' height='12' title='Next'/></a>" : "<img src='images/next.gif' border='0' width='12' height='12'/>";?>
+              <?echo ($page < $pages) ? "<a href='index.php?editor=server&action=6&page=" . $pages . (($sort != "") ? "&sort=" . $sort : "")  . (($filter['status'] == "on") ? $filter['url'] : "") . "'><img src='images/last.gif' border='0' width='12' height='12' title='Last'/></a>" : "<img src='images/last.gif' border='0' width='12' height='12'/>";?>
             </td>
-              </tr>
-            </table>
-          </div>
-          <table class="table_content2" width="100%">
+            <td align="right" width="33%">
+              <a onClick="document.getElementById('filter_box').style.display='block';"><img src="images/filter.jpg" border="0" height="13" width="13" title="Show filter"></a>&nbsp;<a onClick="toggleMultiDelete();"><img id="multiD" src="images/remove3.gif" border="0" height="13" width="13" title="Delete multiple"></a><a id="select_all" onClick="toggleSelectAll();" style="display:none;">Select All</a>
+            </td>
+          </tr>
+        </table>
+      </div>
+      <table class="table_content2" width="100%">
 <?if (isset($hackers)) :?>
-            <tr>
-              <td align="center"><strong><?echo ($sort == 1) ? "ID <img src='images/sort_red.bmp' border='0' width='8' height='8'/>" : "<a href='index.php?editor=server&action=6&sort=1" . (($filter['status'] == 'on') ? $filter['url'] : '') . "'>ID <img src='images/sort_green.bmp' border='0' width='8' height='8' title='Sort by ID'/></a>";?></strong></td>
-              <td align="center"><strong><?echo ($sort == 2) ? "Account <img src='images/sort_red.bmp' border='0' width='8' height='8'/>" : "<a href='index.php?editor=server&action=6&sort=2" . (($filter['status'] == 'on') ? $filter['url'] : '') . "'>Account <img src='images/sort_green.bmp' border='0' width='8' height='8' title='Sort by Account'/></a>";?></strong></td>
-              <td align="center"><strong><?echo ($sort == 3) ? "Name <img src='images/sort_red.bmp' border='0' width='8' height='8'/>" : "<a href='index.php?editor=server&action=6&sort=3" . (($filter['status'] == 'on') ? $filter['url'] : '') . "'>Name <img src='images/sort_green.bmp' border='0' width='8' height='8' title='Sort by Name'/></a>";?></strong></td>
-              <td align="center"><strong><?echo ($sort == 4) ? "Zone <img src='images/sort_red.bmp' border='0' width='8' height='8'/>" : "<a href='index.php?editor=server&action=6&sort=4" . (($filter['status'] == 'on') ? $filter['url'] : '') . "'>Zone <img src='images/sort_green.bmp' border='0' width='8' height='8' title='Sort by Zone'/></a>";?></strong></td>
-              <td align="center"><strong><?echo ($sort == 5) ? "Date <img src='images/sort_red.bmp' border='0' width='8' height='8'/>" : "<a href='index.php?editor=server&action=6&sort=5" . (($filter['status'] == 'on') ? $filter['url'] : '') . "'>Date <img src='images/sort_green.bmp' border='0' width='8' height='8' title='Sort by Date'/></a>";?></strong></td>
-              <td align="center"><strong><?echo ($sort == 6) ? "Hack <img src='images/sort_red.bmp' border='0' width='8' height='8'/>" : "<a href='index.php?editor=server&action=6&sort=6" . (($filter['status'] == 'on') ? $filter['url'] : '') . "'>Hack <img src='images/sort_green.bmp' border='0' width='8' height='8' title='Sort by Hack'/></a>";?></strong></td>
-              <td width="5%">&nbsp;</td>
-            </tr>
+        <tr>
+          <td align="center"><strong><?echo ($sort == 1) ? "ID <img src='images/sort_red.bmp' border='0' width='8' height='8'/>" : "<a href='index.php?editor=server&action=6&sort=1" . (($filter['status'] == 'on') ? $filter['url'] : '') . "'>ID <img src='images/sort_green.bmp' border='0' width='8' height='8' title='Sort by ID'/></a>";?></strong></td>
+          <td align="center"><strong><?echo ($sort == 2) ? "Account <img src='images/sort_red.bmp' border='0' width='8' height='8'/>" : "<a href='index.php?editor=server&action=6&sort=2" . (($filter['status'] == 'on') ? $filter['url'] : '') . "'>Account <img src='images/sort_green.bmp' border='0' width='8' height='8' title='Sort by Account'/></a>";?></strong></td>
+          <td align="center"><strong><?echo ($sort == 3) ? "Name <img src='images/sort_red.bmp' border='0' width='8' height='8'/>" : "<a href='index.php?editor=server&action=6&sort=3" . (($filter['status'] == 'on') ? $filter['url'] : '') . "'>Name <img src='images/sort_green.bmp' border='0' width='8' height='8' title='Sort by Name'/></a>";?></strong></td>
+          <td align="center"><strong><?echo ($sort == 4) ? "Zone <img src='images/sort_red.bmp' border='0' width='8' height='8'/>" : "<a href='index.php?editor=server&action=6&sort=4" . (($filter['status'] == 'on') ? $filter['url'] : '') . "'>Zone <img src='images/sort_green.bmp' border='0' width='8' height='8' title='Sort by Zone'/></a>";?></strong></td>
+          <td align="center"><strong><?echo ($sort == 5) ? "Date <img src='images/sort_red.bmp' border='0' width='8' height='8'/>" : "<a href='index.php?editor=server&action=6&sort=5" . (($filter['status'] == 'on') ? $filter['url'] : '') . "'>Date <img src='images/sort_green.bmp' border='0' width='8' height='8' title='Sort by Date'/></a>";?></strong></td>
+          <td align="center"><strong><?echo ($sort == 6) ? "Hack <img src='images/sort_red.bmp' border='0' width='8' height='8'/>" : "<a href='index.php?editor=server&action=6&sort=6" . (($filter['status'] == 'on') ? $filter['url'] : '') . "'>Hack <img src='images/sort_green.bmp' border='0' width='8' height='8' title='Sort by Hack'/></a>";?></strong></td>
+          <td width="10%">&nbsp;</td>
+        </tr>
 <?$x=0; foreach($hackers as $hackers=>$v):?>
-            <tr bgcolor="#<? echo ($x % 2 == 0) ? "BBBBBB" : "AAAAAA";?>">
-              <td align="center" width="5%"><?=$v['hid']?></td>
-              <td align="center" width="10%"><a href="index.php?editor=account&acctid=<?echo getAccountID($v['account'])?>"><?=$v['account']?></a></td>
-              <td align="center" width="15%"><a href="index.php?editor=player&playerid=<?echo getPlayerID($v['name'])?>"><?=$v['name']?></a></td>
-              <td align="center" width="15%"><?echo ($v['zone']) ? $v['zone'] : "N/A";?></td>
-              <td align="center" width="20%"><?=$v['date']?></td>
-              <td align="center" width="30%"><?=substr($v['hacked'],0,23)?></td>
-              <td align="right"><a href="index.php?editor=server&hid=<?=$v['hid']?>&action=8"><img src="images/edit2.gif" border="0" title="View Hacker"></a>&nbsp;<a onClick="return confirm('Really Delete Entry <?=$v['hid']?>?');" href="index.php?editor=server&hid=<?=$v['hid']?>&action=7"><img src="images/remove3.gif" border="0" title="Delete this entry"></a></td>
-            </tr>
+        <tr bgcolor="#<? echo ($x % 2 == 0) ? "BBBBBB" : "AAAAAA";?>">
+          <td align="center" width="5%"><?=$v['hid']?></td>
+          <td align="center" width="10%"><a href="index.php?editor=account&acctid=<?echo getAccountID($v['account'])?>"><?=$v['account']?></a></td>
+          <td align="center" width="15%"><a href="index.php?editor=player&playerid=<?echo getPlayerID($v['name'])?>"><?=$v['name']?></a></td>
+          <td align="center" width="15%"><?echo ($v['zone']) ? $v['zone'] : "N/A";?></td>
+          <td align="center" width="20%"><?=$v['date']?></td>
+          <td align="center" width="25%"><?=substr($v['hacked'],0,23)?></td>
+          <td align="right"><a href="index.php?editor=server&hid=<?=$v['hid']?>&action=8"><img src="images/edit2.gif" border="0" title="View Hacker"></a>&nbsp;<a onClick="return confirm('Really Delete Entry <?=$v['hid']?>?');" href="index.php?editor=server&hid=<?=$v['hid']?>&action=7"><img name="delete_entry" src="images/remove3.gif" border="0" title="Delete this entry"></a><input name="cb_delete" type="checkbox" value="<?=$v['hid']?>" style="display:none;"></td>
+        </tr>
 <?$x++; endforeach;?>
 <?endif;?>
 <?if (!isset($hackers)):?>
-            <tr>
-              <td align="left" width="100" style="padding: 10px;">No hackers</td>
-            </tr>
+        <tr>
+          <td align="left" width="100" style="padding: 10px;">No hackers</td>
+        </tr>
 <?endif;?>
-          </table>
-        </div>
+      </table>
+    </div>
+    <div id="action_buttons_bottom" style="display:none;">
+      <br/><center><input type="button" value="Delete Marked" onClick="pushIdArray();">&nbsp;<input type="button" value="Cancel" onClick="location.reload();"></center>
+    </div>
+    <form name="deleteHacks" id="deleteHacks" method="post" action="index.php?editor=server&action=49">
+      <input type="hidden" id="ids" name="ids" value="">
+    </form>
--- templates/server/js.tmpl.php
+++ templates/server/js.tmpl.php
@@ -10,4 +10,52 @@
         document.getElementById("note").disabled = true;
       }
     }
+    function toggleMultiDelete() {
+      //Hide delete images
+      document.getElementById("multiD").style.display = "none";
+      var dElements = document.getElementsByName("delete_entry");
+      for (var x = 0; x < dElements.length; x++) {
+        dElements[x].style.display = "none";
+      }
+      //Show checkboxes
+      var cbElements = document.getElementsByName("cb_delete");
+      for (var x = 0; x < cbElements.length; x++) {
+        cbElements[x].style.display = "inline";
+      }
+      //Show form buttons and select_all link
+      document.getElementById("action_buttons_top").style.display = "inline";
+      document.getElementById("action_buttons_bottom").style.display = "inline";
+      document.getElementById("select_all").style.display = "inline";
+    }
+    function toggleSelectAll() {
+      if (document.getElementById("select_all").innerHTML == "Select All") {
+        //Select all checkboxes
+        var cbElements = document.getElementsByName("cb_delete");
+        for (var x = 0; x < cbElements.length; x++) {
+          cbElements[x].checked = true;
+        }
+        //Change hyperlink
+        document.getElementById("select_all").innerHTML = "Deselect All";
+      }
+      else {
+        //Deselect all checkboxes
+        var cbElements = document.getElementsByName("cb_delete");
+        for (var x = 0; x < cbElements.length; x++) {
+          cbElements[x].checked = false;
+        }
+        //Change hyperlink
+        document.getElementById("select_all").innerHTML = "Select All";
+      }
+    }
+    function pushIdArray() {
+      var id_array = new Array();
+      var cbElements = document.getElementsByName("cb_delete");
+      for (var x = 0; x < cbElements.length; x++) {
+        if (cbElements[x].checked) {
+          id_array.push(cbElements[x].value);
+        }
+      }
+      document.getElementById("ids").value = JSON.stringify(id_array);
+      document.getElementById("deleteHacks").submit();
+    }
   </script>
