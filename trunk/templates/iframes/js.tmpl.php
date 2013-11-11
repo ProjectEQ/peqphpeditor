c087da28d9dde3764f7abddbfe15e4f7390a0120
jeremiah.oligario <jeremiah.oligario@e2836f04-be38-11dd-ab14-3be0eea8eb7e>
2013-11-11 23:33:26 +0000

    Support augment types up to 30. Append "Copy" to copied spells and items. Some code cleanup and spelling fixes.

--- trunk/templates/iframes/js.tmpl.php
+++ trunk/templates/iframes/js.tmpl.php
@@ -1,124 +1,129 @@
 <script type="text/javascript">
-function showSearch() {
-        document.getElementById("searchframe").style.display = "block";
-        document.getElementById("button").style.display = "block";
-      }
+  function showSearch() {
+    document.getElementById("searchframe").style.display = "block";
+    document.getElementById("button").style.display = "block";
+  }
 
-      function hideSearch(name) {
-        document.getElementById("searchframe").style.display = "none";
-        document.getElementById("button").style.display = "none";
-      }
+  function hideSearch(name) {
+    document.getElementById("searchframe").style.display = "none";
+    document.getElementById("button").style.display = "none";
+  }
 
-function Check(form)
-{
-form.slot_Charm.checked = form.all_none.checked ;
-form.slot_Ear01.checked = form.all_none.checked ;
-form.slot_Head.checked = form.all_none.checked ; 
-form.slot_Face.checked = form.all_none.checked ; 
-form.slot_Ear02.checked = form.all_none.checked ;
-form.slot_Neck.checked = form.all_none.checked ; 
-form.slot_Shoulder.checked = form.all_none.checked ; 
-form.slot_Arms.checked = form.all_none.checked ;
-form.slot_Back.checked = form.all_none.checked ; 
-form.slot_Bracer01.checked = form.all_none.checked ;
-form.slot_Bracer02.checked = form.all_none.checked ;
-form.slot_Range.checked = form.all_none.checked ;
-form.slot_Hands.checked = form.all_none.checked ; 
-form.slot_Primary.checked = form.all_none.checked ; 
-form.slot_Secondary.checked = form.all_none.checked ; 
-form.slot_Ring01.checked = form.all_none.checked ; 
-form.slot_Ring02.checked = form.all_none.checked ;
-form.slot_Chest.checked = form.all_none.checked ; 
-form.slot_Legs.checked = form.all_none.checked ; 
-form.slot_Feet.checked = form.all_none.checked ; 
-form.slot_Waist.checked = form.all_none.checked ;
-form.slot_Ammo.checked = form.all_none.checked ; 
-form.slot_Powersource.checked = form.all_none.checked ;
-}
+  function Check(form) {
+    form.slot_Charm.checked = form.all_none.checked;
+    form.slot_Ear01.checked = form.all_none.checked;
+    form.slot_Head.checked = form.all_none.checked;
+    form.slot_Face.checked = form.all_none.checked;
+    form.slot_Ear02.checked = form.all_none.checked;
+    form.slot_Neck.checked = form.all_none.checked;
+    form.slot_Shoulder.checked = form.all_none.checked;
+    form.slot_Arms.checked = form.all_none.checked;
+    form.slot_Back.checked = form.all_none.checked;
+    form.slot_Bracer01.checked = form.all_none.checked;
+    form.slot_Bracer02.checked = form.all_none.checked;
+    form.slot_Range.checked = form.all_none.checked;
+    form.slot_Hands.checked = form.all_none.checked;
+    form.slot_Primary.checked = form.all_none.checked;
+    form.slot_Secondary.checked = form.all_none.checked;
+    form.slot_Ring01.checked = form.all_none.checked;
+    form.slot_Ring02.checked = form.all_none.checked;
+    form.slot_Chest.checked = form.all_none.checked;
+    form.slot_Legs.checked = form.all_none.checked;
+    form.slot_Feet.checked = form.all_none.checked;
+    form.slot_Waist.checked = form.all_none.checked;
+    form.slot_Ammo.checked = form.all_none.checked;
+    form.slot_Powersource.checked = form.all_none.checked;
+  }
 
-function Check2(form)
-{
-form.race_Human.checked = form.all_none2.checked ;
-form.race_Barbarian.checked = form.all_none2.checked ;
-form.race_Erudite.checked = form.all_none2.checked ;
-form.race_Wood_Elf.checked = form.all_none2.checked ; 
-form.race_High_Elf.checked = form.all_none2.checked ; 
-form.race_Dark_Elf.checked = form.all_none2.checked ; 
-form.race_Half_Elf.checked = form.all_none2.checked ; 
-form.race_Dwarf.checked = form.all_none2.checked ;
-form.race_Troll.checked = form.all_none2.checked ;
-form.race_Ogre.checked = form.all_none2.checked ;
-form.race_Halfling.checked = form.all_none2.checked ; 
-form.race_Gnome.checked = form.all_none2.checked ;
-form.race_Iksar.checked = form.all_none2.checked ;
-form.race_Vah_Shir.checked = form.all_none2.checked ; 
-form.race_Froglok.checked = form.all_none2.checked ;
-form.race_Drakkin.checked = form.all_none2.checked ;
-form.race_Shroud.checked = form.all_none2.checked ;
-}
+function Check2(form) {
+    form.race_Human.checked = form.all_none2.checked;
+    form.race_Barbarian.checked = form.all_none2.checked;
+    form.race_Erudite.checked = form.all_none2.checked;
+    form.race_Wood_Elf.checked = form.all_none2.checked;
+    form.race_High_Elf.checked = form.all_none2.checked;
+    form.race_Dark_Elf.checked = form.all_none2.checked;
+    form.race_Half_Elf.checked = form.all_none2.checked;
+    form.race_Dwarf.checked = form.all_none2.checked;
+    form.race_Troll.checked = form.all_none2.checked;
+    form.race_Ogre.checked = form.all_none2.checked;
+    form.race_Halfling.checked = form.all_none2.checked;
+    form.race_Gnome.checked = form.all_none2.checked;
+    form.race_Iksar.checked = form.all_none2.checked;
+    form.race_Vah_Shir.checked = form.all_none2.checked;
+    form.race_Froglok.checked = form.all_none2.checked;
+    form.race_Drakkin.checked = form.all_none2.checked;
+    form.race_Shroud.checked = form.all_none2.checked;
+  }
 
-function Check3(form)
-{
-form.class_Warrior.checked = form.all_none3.checked ;
-form.class_Cleric.checked = form.all_none3.checked ;
-form.class_Paladin.checked = form.all_none3.checked ;
-form.class_Ranger.checked = form.all_none3.checked ; 
-form.class_Shadowknight.checked = form.all_none3.checked ;
-form.class_Druid.checked = form.all_none3.checked ;
-form.class_Monk.checked = form.all_none3.checked ; 
-form.class_Bard.checked = form.all_none3.checked ; 
-form.class_Rogue.checked = form.all_none3.checked ;
-form.class_Shaman.checked = form.all_none3.checked ;
-form.class_Necromancer.checked = form.all_none3.checked ;
-form.class_Wizard.checked = form.all_none3.checked ;
-form.class_Magician.checked = form.all_none3.checked ;
-form.class_Enchanter.checked = form.all_none3.checked ;
-form.class_Beastlord.checked = form.all_none3.checked ;
-form.class_Berserker.checked = form.all_none3.checked ;
-}
+  function Check3(form) {
+    form.class_Warrior.checked = form.all_none3.checked;
+    form.class_Cleric.checked = form.all_none3.checked;
+    form.class_Paladin.checked = form.all_none3.checked;
+    form.class_Ranger.checked = form.all_none3.checked;
+    form.class_Shadowknight.checked = form.all_none3.checked;
+    form.class_Druid.checked = form.all_none3.checked;
+    form.class_Monk.checked = form.all_none3.checked;
+    form.class_Bard.checked = form.all_none3.checked;
+    form.class_Rogue.checked = form.all_none3.checked;
+    form.class_Shaman.checked = form.all_none3.checked;
+    form.class_Necromancer.checked = form.all_none3.checked;
+    form.class_Wizard.checked = form.all_none3.checked;
+    form.class_Magician.checked = form.all_none3.checked;
+    form.class_Enchanter.checked = form.all_none3.checked;
+    form.class_Beastlord.checked = form.all_none3.checked;
+    form.class_Berserker.checked = form.all_none3.checked;
+  }
 
-function Check4(form)
-{
-form.deity_Agnostic.checked = form.all_none4.checked ;
-form.deity_Bertox.checked = form.all_none4.checked ;
-form.deity_Brell.checked = form.all_none4.checked ;
-form.deity_Cazic.checked = form.all_none4.checked ;
-form.deity_Erollsi.checked = form.all_none4.checked ;
-form.deity_Bristlebane.checked = form.all_none4.checked ;
-form.deity_Innoruuk.checked = form.all_none4.checked ;
-form.deity_Karana.checked = form.all_none4.checked ;
-form.deity_Mithaniel_Marr.checked = form.all_none4.checked ;
-form.deity_Prexus.checked = form.all_none4.checked ;
-form.deity_Quellious.checked = form.all_none4.checked ;
-form.deity_Rallos_Zek.checked = form.all_none4.checked ; 
-form.deity_Rodcet_Nife.checked = form.all_none4.checked ;
-form.deity_Solusek_Ro.checked = form.all_none4.checked ; 
-form.deity_The_Tribunal.checked = form.all_none4.checked ;
-form.deity_Tunare.checked = form.all_none4.checked ;
-form.deity_Veeshan.checked = form.all_none4.checked ;
-}
+  function Check4(form) {
+    form.deity_Agnostic.checked = form.all_none4.checked;
+    form.deity_Bertox.checked = form.all_none4.checked;
+    form.deity_Brell.checked = form.all_none4.checked;
+    form.deity_Cazic.checked = form.all_none4.checked;
+    form.deity_Erollsi.checked = form.all_none4.checked;
+    form.deity_Bristlebane.checked = form.all_none4.checked;
+    form.deity_Innoruuk.checked = form.all_none4.checked;
+    form.deity_Karana.checked = form.all_none4.checked;
+    form.deity_Mithaniel_Marr.checked = form.all_none4.checked;
+    form.deity_Prexus.checked = form.all_none4.checked;
+    form.deity_Quellious.checked = form.all_none4.checked;
+    form.deity_Rallos_Zek.checked = form.all_none4.checked;
+    form.deity_Rodcet_Nife.checked = form.all_none4.checked;
+    form.deity_Solusek_Ro.checked = form.all_none4.checked;
+    form.deity_The_Tribunal.checked = form.all_none4.checked;
+    form.deity_Tunare.checked = form.all_none4.checked;
+    form.deity_Veeshan.checked = form.all_none4.checked;
+  }
 
-function Check5(form)
-{
-form.augtype_Type_1.checked = form.all_none5.checked ; 
-form.augtype_Type_2.checked = form.all_none5.checked ; 
-form.augtype_Type_3.checked = form.all_none5.checked ; 
-form.augtype_Type_4.checked = form.all_none5.checked ; 
-form.augtype_Type_5.checked = form.all_none5.checked ; 
-form.augtype_Type_6.checked = form.all_none5.checked ; 
-form.augtype_Type_7.checked = form.all_none5.checked ; 
-form.augtype_Type_8.checked = form.all_none5.checked ; 
-form.augtype_Type_9.checked = form.all_none5.checked ; 
-form.augtype_Type_10.checked = form.all_none5.checked ;
-form.augtype_Type_11.checked = form.all_none5.checked ;
-form.augtype_Type_12.checked = form.all_none5.checked ;
-form.augtype_Type_13.checked = form.all_none5.checked ;
-form.augtype_Type_14.checked = form.all_none5.checked ;
-form.augtype_Type_15.checked = form.all_none5.checked ;
-form.augtype_Type_16.checked = form.all_none5.checked ;
-form.augtype_Type_17.checked = form.all_none5.checked ;
-form.augtype_Type_18.checked = form.all_none5.checked ;
-form.augtype_Type_19.checked = form.all_none5.checked ;
-form.augtype_Type_20.checked = form.all_none5.checked ;
-}
+  function Check5(form) {
+    form.augtype_Type_1.checked = form.all_none5.checked;
+    form.augtype_Type_2.checked = form.all_none5.checked;
+    form.augtype_Type_3.checked = form.all_none5.checked;
+    form.augtype_Type_4.checked = form.all_none5.checked;
+    form.augtype_Type_5.checked = form.all_none5.checked;
+    form.augtype_Type_6.checked = form.all_none5.checked;
+    form.augtype_Type_7.checked = form.all_none5.checked;
+    form.augtype_Type_8.checked = form.all_none5.checked;
+    form.augtype_Type_9.checked = form.all_none5.checked;
+    form.augtype_Type_10.checked = form.all_none5.checked;
+    form.augtype_Type_11.checked = form.all_none5.checked;
+    form.augtype_Type_12.checked = form.all_none5.checked;
+    form.augtype_Type_13.checked = form.all_none5.checked;
+    form.augtype_Type_14.checked = form.all_none5.checked;
+    form.augtype_Type_15.checked = form.all_none5.checked;
+    form.augtype_Type_16.checked = form.all_none5.checked;
+    form.augtype_Type_17.checked = form.all_none5.checked;
+    form.augtype_Type_18.checked = form.all_none5.checked;
+    form.augtype_Type_19.checked = form.all_none5.checked;
+    form.augtype_Type_20.checked = form.all_none5.checked;
+    form.augtype_Type_21.checked = form.all_none5.checked;
+    form.augtype_Type_22.checked = form.all_none5.checked;
+    form.augtype_Type_23.checked = form.all_none5.checked;
+    form.augtype_Type_24.checked = form.all_none5.checked;
+    form.augtype_Type_25.checked = form.all_none5.checked;
+    form.augtype_Type_26.checked = form.all_none5.checked;
+    form.augtype_Type_27.checked = form.all_none5.checked;
+    form.augtype_Type_28.checked = form.all_none5.checked;
+    form.augtype_Type_29.checked = form.all_none5.checked;
+    form.augtype_Type_30.checked = form.all_none5.checked;
+  }
 </script>
