5a1a0fe6ad4da67a18d712fac7288ac17c4540fe
jeremiah.oligario <jeremiah.oligario@e2836f04-be38-11dd-ab14-3be0eea8eb7e>
2013-11-24 05:29:50 +0000

    Re-add function to move characters between accounts.

--- templates/account/account.chartransfer.tmpl.php
+++ templates/account/account.chartransfer.tmpl.php
@@ -1,17 +1,28 @@
+<?
+  $from_acct = getAccountName($acctid);
+?>
+  <div id="searchblock" style="display:none;">
+    <center>
+      <iframe src="templates/iframes/accountsearch.php" style="display:block;"></iframe>
+      <input type="button" value="Hide Search" onclick="hideSearch();">
+    </center><br/>
+  </div>
   <div class="table_container" style="width:250px;">
     <div class="table_header">
       Character Transfer
     </div>
     <div class="table_content">
-      <center>
-        Transfer <strong><font color="red"><?=getPlayerName($_GET['playerid'])?></font></strong> from<br/><br/>
-        <strong><?=getAccountName($acctid)?></strong><br/><br/>
-        to<br/><br/>
-        <select onChange="gotosite(this.options[this.selectedIndex].value);">>
-<?foreach ($target_accounts as $target_account): extract($target_account);?>
-          <option value="index.php?editor=account&acctid=<?=$acctid?>&tacct=<?=$target_account['id']?>&playerid=<?=$_GET['playerid']?>&action=6"<?if ($acctid == $target_account['id']) echo ' selected';?>><?=$target_account['name']?></option>
-<?endforeach;?>
-        </select>
-      </center>
+      <form id="transfer" method="post" action="index.php?editor=account&acctid=<?=$acctid?>&playerid=<?=$_GET['playerid']?>&action=6">
+        <center>
+          Transfer <strong><font color="green"><?=getPlayerName($_GET['playerid'])?></font></strong> from<br/><br/>
+          <strong><?=$from_acct?></strong><br/><br/>
+          to<br/><br/>
+          <input type="text" size="20" value="Select account" readonly="true" id="to_text" name="tacct" onClick="showSearch();">
+          <input type="hidden" id="from_acct" value="<?=$from_acct?>">
+          <div id="submitblock" style="display:none;">
+            <br/><br/><input type="button" value="GO" onClick="validateTransfer();">&nbsp;<input type="button" onClick="history.go(-2);" value="Cancel">
+          </div>
+        </center>
+      </form>
     </div>
   </div>
---
+++ templates/account/js.tmpl.php
@@ -0,0 +1,22 @@
+<script language="javascript">
+  function showSearch() {
+    document.getElementById("searchblock").style.display = "block";
+  }
+  function hideSearch() {
+    document.getElementById("searchblock").style.display = "none";
+  }
+  function prepareTransfer() {
+    document.getElementById("to_text").style.textDecoration = "none";
+    document.getElementById("submitblock").style.display = "block";
+  }
+  function validateTransfer() {
+    from_acct = document.getElementById("from_acct").value;
+    to_acct = document.getElementById("to_text").value;
+    if (from_acct == to_acct) {
+      alert("You cannot transfer to the same account!");
+    }
+    else {
+      document.getElementById("transfer").submit();
+    }
+  }
+</script>
