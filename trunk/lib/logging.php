9f259c9012c3374caf5a17ede9ae5e2b7e231a30
cavedude00@gmail.com <cavedude00@gmail.com@e2836f04-be38-11dd-ab14-3be0eea8eb7e>
2014-05-21 18:25:54 +0000

    spawn_events now sort by ID. Magelo stuff since I made the supporting DB pulic.

--- trunk/lib/logging.php
+++ trunk/lib/logging.php
@@ -28,4 +28,31 @@ function logSQL ($query) {
   }
 }
 
+function logPerl ($query) {
+  global $perl_log_file, $logging;
+  $user = $_SESSION['login'];
+
+  if (isset($_SESSION['guest']) && ($_SESSION['guest'] == 1)) {
+    $user = 'Guest';
+  }
+  if ($user == '') {
+    $user = 'N/A';
+  }
+
+  if ($logging == 1) {
+    $time = date("j-M-y  G:i:s");
+    if (!is_writable($perl_log_file)) {
+      $handle = fopen($perl_log_file, 'w') or die("Could not create $perl_log_file! Make sure the logs directory is writeable by your webserver.");
+    }
+    if (!$handle = fopen($perl_log_file, 'a')) {
+      echo "Unable to open the log file ($perl_log_file)! Make sure the file is readable by your webserver.";
+      exit;
+    }
+    if (!fwrite($handle, "$query ($user $time)\r\n")) {
+      echo "Could not write to the log file ($perl_log_file)! Make sure the file is writeable by your webserver.";
+      exit;
+    }
+    fclose($handle);
+  }
+}
 ?>
