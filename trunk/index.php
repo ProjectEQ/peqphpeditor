bbedcd37e0f40798bf42c9e3e38835b081a9a5ba
jeremiah.oligario <jeremiah.oligario@e2836f04-be38-11dd-ab14-3be0eea8eb7e>
2014-05-26 18:20:33 +0000

    Added Cabilis Forge to list of common containers.

--- trunk/index.php
+++ trunk/index.php
@@ -1,6 +1,6 @@
 <?php
 
-$current_revision = "r431";
+$current_revision = "r434";
 
 require_once("config.php");
 require_once("lib/logging.php");
--- trunk/templates/tradeskill/component.add.tmpl.php
+++ trunk/templates/tradeskill/component.add.tmpl.php
@@ -2,28 +2,24 @@
         <iframe id='searchframe' src='templates/iframes/itemsearch.php'></iframe>
         <input id="button" type="button" value='Hide Item Search' onclick='hideSearch();' style='display:none; margin-bottom: 20px;'>
       </center>
-
       <form method="post" action="index.php?editor=tradeskill&ts=<?=$ts?>&rec=<?=$rec?>&action=8">
-      <div class="edit_form" style="width: 250px;">
-        <div class="edit_form_header">
-          Add Recipe Component
-        </div>
-        <div class="edit_form_content">
+        <div class="edit_form" style="width: 250px;">
+          <div class="edit_form_header">Add Recipe Component</div>
+          <div class="edit_form_content">
             <strong>Item ID:</strong> (<a href="javascript:showSearch();">search</a>)<br>
             <input class="indented" id="id" type="text" name="item_id" size="7" value=""><br><br>
-
             <strong>Item is a:</strong><br>
             <select class="indented" name="type" id="type" onChange="toggleComponentType()">
               <option value="0">Combine Container</option>
               <option value="1">Component</option>
               <option value="2">Product</option>
             </select><br><br>
-			<table width="100%">
+            <table width="100%">
               <tr>
-			    <td align="center" width="5%"><a href="javascript:toggleContainer();" id="ContainerCollapsed" style="display:inline;" name="Show Common Containers">[+]</a><a href="javascript:toggleContainer();" id="ContainerExpanded" style="display:none;" name="Show Common Containers">[-]</a></td>
-			    <td width="30%"><strong>Common Containers</strong></td>
-			  </tr>
-			</table><br>
+                <td align="center" width="5%"><a href="javascript:toggleContainer();" id="ContainerCollapsed" style="display:inline;" name="Show Common Containers">[+]</a><a href="javascript:toggleContainer();" id="ContainerExpanded" style="display:none;" name="Show Common Containers">[-]</a></td>
+                <td width="30%"><strong>Common Containers</strong></td>
+              </tr>
+            </table><br>
             <table id="ContainerTable" style="display:none;" width="100%">
               <tr>
                 <td align="center" width="10%"><strong>ID</strong></td>
@@ -90,6 +86,10 @@
                 <td width="30%">Oggok Forge</td>
               </tr>
               <tr>
+                <td align="center" width="10%"><a href="javascript:document.getElementById('id').value='38';javascript:document.getElementById('type').value='0';javascript:toggleContainer();">38</a></td>
+                <td width="30%">Cabilis Forge</td>
+              </tr>
+              <tr>
                 <td align="center" width="10%"><a href="javascript:document.getElementById('id').value='46';javascript:document.getElementById('type').value='0';javascript:toggleContainer();">46</a></td>
                 <td width="30%">Tackle Box</td>
               </tr>
@@ -104,25 +104,23 @@
             </table>
             <fieldset>
               <legend><strong><font size="-1">Components:</font></strong></legend>
-		    			Qty Required:<br>
+              Qty Required:<br>
               <input class="indented" type="text" name="componentcount" size="7" value="0" disabled><br><br>
-              Qty Returned on Fail: <br>
+              Qty Returned on Fail:<br>
               <input class="indented" type="text" name="failcount" size="7" value="0" disabled><br><br>
-		Qty Returned on Salvage: <br>
+              Qty Returned on Salvage:<br>
               <input class="indented" type="text" name="salvagecount" size="7" value="0" disabled>
             </fieldset><br><br>
-
             <fieldset>
               <legend><strong><font size="-1">Products:</font></strong></legend>
               Qty Produced: <br>
               <input class="indented" type="text" name="successcount" size="7" value="0" disabled>
             </fieldset><br>
-
             <center>
               <input type="hidden" name='iscontainer' value="1">
               <input type="hidden" name="recipe_id" value="<?=$rec?>">
               <input type="submit" name="submit" value="Submit Changes" onClick="enable();">
             </center>
+          </div>
         </div>
-      </div>
       </form>
