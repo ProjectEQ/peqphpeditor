ac102cc93741c161e7b2ac0e3524b74d5b419b62
jeremiah.oligario <jeremiah.oligario@e2836f04-be38-11dd-ab14-3be0eea8eb7e>
2014-09-11 03:07:51 +0000

    Minor HTML cleanup.

--- trunk/index.php
+++ trunk/index.php
@@ -1,6 +1,6 @@
 <?php
 
-$current_revision = "r438";
+$current_revision = "r440";
 
 require_once("config.php");
 require_once("lib/logging.php");
--- trunk/templates/npc/npc.changelevelver.tmpl.php
+++ trunk/templates/npc/npc.changelevelver.tmpl.php
@@ -1,25 +1,28 @@
-       <table class="edit_form">
-         <tr>
-           <td class="edit_form_header">
-             Change NPC Levels
-           </td>
-         </tr>
-         <tr>
-           <td class="edit_form_content">
-             <form name="npc_level" method="post" action="index.php?editor=npc&z=<?=$currzone?>&zoneid=<?=$currzoneid?>&action=46">
-             <table width="100%">
-             <tr>
-             <th>Version:</th>
-             <th>Level Diff:</th>
-             </tr>
-             <tr>
-             <td><input type="text" size="7" name="npc_version" value="0"></td>
-             <td><input type="text" size="7" name="npc_level" value="0"></td>
-             </tr>
-            <table>
-             <center><br>
-               <input type="submit" value="Submit"></form><br><br>
-             </center>
-           </td>
-         </tr>
-       </table>
+  <table class="edit_form">
+    <tr>
+      <td class="edit_form_header">Change NPC Levels</td>
+    </tr>
+    <tr>
+      <td class="edit_form_content">
+        <form name="npc_level" method="post" action="index.php?editor=npc&z=<?=$currzone?>&zoneid=<?=$currzoneid?>&action=46">
+          <table width="100%">
+            <tr>
+              <th>Version:</th>
+              <th>Level Diff:</th>
+            </tr>
+            <tr>
+              <td><input type="text" size="7" name="npc_version" value="0"></td>
+              <td><input type="text" size="7" name="npc_level" value="0"></td>
+            </tr>
+            <tr>
+              <td>
+                <center>
+                  <input type="submit" value="Submit"><br/><br/>
+                </center>
+              </td>
+            </tr>
+          </table>
+        </form>
+      </td>
+    </tr>
+  </table>
