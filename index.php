7b87504a44782fc292948a5f1eb8ccf6cf2c4d27
jeremiah.oligario <jeremiah.oligario@e2836f04-be38-11dd-ab14-3be0eea8eb7e>
2014-09-25 16:43:14 +0000

    A little more intuitive on tradeskill menu.

--- index.php
+++ index.php
@@ -1,6 +1,6 @@
 <?php
 
-$current_revision = "r446";
+$current_revision = "r447";
 
 require_once("config.php");
 require_once("lib/logging.php");
--- templates/tradeskill/tradeskill.default.tmpl.php
+++ templates/tradeskill/tradeskill.default.tmpl.php
@@ -1,12 +1,16 @@
+<?
+  if (isset($ts) && ($ts > 0))
+    $selected_skill = $tradeskills[$ts];
+?>
        <table class="edit_form">
          <tr>
            <td class="edit_form_header">
-             Create a New Recipe
+             Tradeskill Options
            </td>
          </tr>
          <tr>
-           <td class="edit_form_content">
-             <a href="index.php?editor=tradeskill&action=10">Create a New Recipe</a><br/>
+           <td class="edit_form_content" align="center">
+             <a href="index.php?editor=tradeskill&action=10<?echo ($selected_skill != '') ? '&ts=' . $ts : '';?>">Create a New <?echo ($selected_skill != '') ? $selected_skill . ' ' : '';?>Recipe</a><br/>
              <a href="index.php?editor=tradeskill&action=13">View Learned Recipes</a>
            </td>
          </tr>
