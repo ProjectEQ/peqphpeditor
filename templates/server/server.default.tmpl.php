be2299321b506f21820128918aa39e379e52f587
jeremiah.oligario <jeremiah.oligario@e2836f04-be38-11dd-ab14-3be0eea8eb7e>
2015-01-16 13:37:53 +0000

    Initial work to add Character Creation Combos functions.

--- templates/server/server.default.tmpl.php
+++ templates/server/server.default.tmpl.php
@@ -1,17 +1,18 @@
-        <table class="edit_form">
-          <tr>
-            <td class="edit_form_header">Server Options</td>
-          </tr>
-          <tr>
-            <td class="edit_form_content">
-              <center><a href="index.php?editor=server&action=1">View Bug Reports</a><br></center>
-              <center><a href="index.php?editor=server&action=6">View Hackers</a><br></center>
-              <center><a href="index.php?editor=server&action=9">View Reported Players</a><br></center>
-              <center><a href="index.php?editor=server&action=12">View Petitions</a><br></center>
-              <center><a href="index.php?editor=server&action=16">View Rules</a><br></center>
-              <center><a href="index.php?editor=server&action=32">View Zones</a><br></center>
-              <center><a href="index.php?editor=server&action=43">View Variables</a><br></center>
-		<center><a href="index.php?editor=server&action=50">View BannedIPs</a><br></center>
-            </td>
-          </tr>
-        </table>
+  <table class="edit_form">
+    <tr>
+      <td class="edit_form_header">Server Options</td>
+    </tr>
+    <tr>
+      <td class="edit_form_content">
+        <center><a href="index.php?editor=server&action=1">View Bug Reports</a><br></center>
+        <center><a href="index.php?editor=server&action=6">View Hackers</a><br></center>
+        <center><a href="index.php?editor=server&action=9">View Reported Players</a><br></center>
+        <center><a href="index.php?editor=server&action=12">View Petitions</a><br></center>
+        <center><a href="index.php?editor=server&action=16">View Rules</a><br></center>
+        <center><a href="index.php?editor=server&action=32">View Zones</a><br></center>
+        <center><a href="index.php?editor=server&action=43">View Variables</a><br></center>
+        <center><a href="index.php?editor=server&action=50">View BannedIPs</a><br></center>
+        <center><a href="index.php?editor=server&action=56">View Character Creation Combos</a><br></center>
+      </td>
+    </tr>
+  </table>
