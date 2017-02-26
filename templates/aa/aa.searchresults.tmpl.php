f9c6d1e2e52aadf4640fd4a2975c69679ac8de9c
tempten <nickdstanley@gmail.com>
2017-02-25 17:43:58 -0800

    Removed redundant commented section.

--- templates/aa/aa.searchresults.tmpl.php
+++ templates/aa/aa.searchresults.tmpl.php
@@ -7,34 +7,6 @@
     return "$expid";
   }
 
-  /*function getClasses($classes, $berserker) {
-    if ($berserker == 0 && $classes == 0) {
-      return "None";
-    }
-    if ($berserker == 1 && $classes == 65534) 
-      return "ALL";
-    else {
-      $res = '';
-      if ($berserker == 1)  $res .= "BER ";
-      if ($classes &   256) $res .= "BRD ";
-      if ($classes & 32768) $res .= "BST ";
-      if ($classes &     4) $res .= "CLR ";
-      if ($classes &    64) $res .= "DRU ";
-      if ($classes & 16384) $res .= "ENC ";
-      if ($classes &  8192) $res .= "MAG ";
-      if ($classes &   128) $res .= "MNK ";
-      if ($classes &  2048) $res .= "NEC ";
-      if ($classes &     8) $res .= "PAL ";
-      if ($classes &    16) $res .= "RNG ";
-      if ($classes &   512) $res .= "ROG ";
-      if ($classes &    32) $res .= "SHD ";
-      if ($classes &  1024) $res .= "SHM ";
-      if ($classes &     2) $res .= "WAR ";
-      if ($classes &  4096) $res .= "WIZ ";
-      $res = rtrim($res, " ");
-      return $res;
-    }
-  }*/
 ?>
 <?if (isset($_GET['action']) && $_GET['action'] == 2):?> 
   <div class="table_container" style="width:350px;">
