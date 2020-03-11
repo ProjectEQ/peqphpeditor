47c6b2a2876dedaa75705a78133955326b23354f
JJ <jeremiah.oligario@gmail.com>
2020-03-10 22:49:01 -0400

    Change default DB engine over to InnoDB

--- sql/schema.sql
+++ sql/schema.sql
@@ -1,9 +1,10 @@
 DROP TABLE IF EXISTS `peq_admin`;
 
-CREATE TABLE peq_admin (
-id INT(11) auto_increment primary key,
-login varchar(30) NOT NULL,
-password varchar(255) NOT NULL,
-administrator INT(11) NOT NULL DEFAULT '0'
-);
-INSERT INTO peq_admin VALUES(1, 'admin', '5f4dcc3b5aa765d61d8327deb882cf99', 1);
+CREATE TABLE `peq_admin` (
+  `id` int(11) AUTO_INCREMENT PRIMARY KEY,
+  `login` varchar(30) NOT NULL,
+  `password` varchar(255) NOT NULL,
+  `administrator` int(11) NOT NULL DEFAULT '0'
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+INSERT INTO peq_admin VALUES(1, 'admin', md5('password'), 1);
