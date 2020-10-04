b970892d052104f33dd2771e2f2d3c2df1b613be
JJ <jeremiah.oligario@gmail.com>
2020-10-03 22:34:11 -0400

    Whoops, wrong table

--- templates/admin/admin.tmpl.php
+++ templates/admin/admin.tmpl.php
@@ -2,7 +2,7 @@
     <br><br><br>
     <h1>Admin Editor</h1>
     <br><br>
-    <div class="table_container" style="width:500px;">
+    <div class="table_container" style="width:350px;">
       <div class="table_header">
         User Management
         <div style="float:right;">
@@ -31,7 +31,7 @@
         </table>
       </div>
     </div><br><br>
-    <div class="table_container" style="width:350px;">
+    <div class="table_container" style="width:500px;">
       <div class="table_header">
         GM IPs
         <div style="float:right;">
