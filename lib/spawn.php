f279fa0ff996dd816d22c06735261de8575b60a9
JJ <jeremiah.oligario@gmail.com>
2020-10-26 19:48:08 -0400

    Fix content flags update for spawnpoints

--- lib/spawn.php
+++ lib/spawn.php
@@ -1397,11 +1397,13 @@ function update_spawnpoint() {
   }
 
   if ($_POST['content_flags'] != "") {
+    $content_flags = $_POST['content_flags'];
     $query = "UPDATE spawn2 SET content_flags=\"$content_flags\" WHERE id=$id";
     $mysql_content_db->query_no_result($query);
   }
 
   if ($_POST['content_flags_disabled'] != "") {
+    $content_flags_disabled = $_POST['content_flags_disabled'];
     $query = "UPDATE spawn2 SET content_flags_disabled=\"$content_flags_disabled\" WHERE id=$id";
     $mysql_content_db->query_no_result($query);
   }
