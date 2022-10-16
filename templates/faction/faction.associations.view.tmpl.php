4f230c2eb8d761940cd025589b5b52c490583757
JJ <jeremiah.oligario@gmail.com>
2022-10-16 18:22:39 -0400

    Clean up faction association view list

--- templates/faction/faction.associations.view.tmpl.php
+++ templates/faction/faction.associations.view.tmpl.php
@@ -1,4 +1,4 @@
-  <div class="table_container" style="width: 750px;">
+  <div class="table_container" style="width: 350px;">
     <div class="table_header">
       <table width="100%" cellpadding="0" cellspacing="0">
         <tr>
@@ -14,27 +14,14 @@
     <table class="table_content2" width="100%">
 <?if (isset($faction_associations)):?>
       <tr>
-        <td align="center"><strong>ID</strong></td>
-        <td align="center"><strong>Factions</strong></td>
-        <td width="10%">&nbsp;</td>
+        <td align="center"><strong>Faction ID</strong></td>
+        <td width="15%">&nbsp;</td>
       </tr>
 <?$x=0;
 foreach($faction_associations as $faction_association):?>
       <tr bgcolor="#<? echo ($x % 2 == 0) ? "BBBBBB" : "AAAAAA";?>">
         <td align="center"><?=getFactionName($faction_association['id'])?> (<?=$faction_association['id']?>)</td>
-        <td align="center">
-          <?=getFactionName($faction_association['id_1'])?> (<?=$faction_association['id_1']?>),
-          <?=getFactionName($faction_association['id_2'])?> (<?=$faction_association['id_2']?>),
-          <?=getFactionName($faction_association['id_3'])?> (<?=$faction_association['id_3']?>),
-          <?=getFactionName($faction_association['id_4'])?> (<?=$faction_association['id_4']?>),
-          <?=getFactionName($faction_association['id_5'])?> (<?=$faction_association['id_5']?>),
-          <?=getFactionName($faction_association['id_6'])?> (<?=$faction_association['id_6']?>),
-          <?=getFactionName($faction_association['id_7'])?> (<?=$faction_association['id_7']?>),
-          <?=getFactionName($faction_association['id_8'])?> (<?=$faction_association['id_8']?>),
-          <?=getFactionName($faction_association['id_9'])?> (<?=$faction_association['id_9']?>),
-          <?=getFactionName($faction_association['id_10'])?> (<?=$faction_association['id_10']?>)
-        </td>
-        <td align="right" width="10%"><a href="index.php?editor=faction&id=<?=$faction_association['id']?>&action=28"><img src="images/edit2.gif" width="13" height="13" border="0" title="Edit Faction Association"></a>&nbsp;<a onClick="return confirm('Really delete faction association <?=$faction_association['id']?>?');" href="index.php?editor=faction&id=<?=$faction_association['id']?>&action=30"><img src="images/remove3.gif" border="0" title="Delete Faction Association"></a></td>
+        <td align="right" width="15%"><a href="index.php?editor=faction&id=<?=$faction_association['id']?>&action=28"><img src="images/edit2.gif" width="13" height="13" border="0" title="Edit Faction Association"></a>&nbsp;<a onClick="return confirm('Really delete faction association <?=$faction_association['id']?>?');" href="index.php?editor=faction&id=<?=$faction_association['id']?>&action=30"><img src="images/remove3.gif" border="0" title="Delete Faction Association"></a></td>
       </tr>
 <?$x++;
 endforeach;
