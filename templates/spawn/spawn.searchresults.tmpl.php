f3d8569274d2a7bfa82f342c7a654a3b284cc5e0
JJ <jeremiah.oligario@gmail.com>
2024-01-04 20:04:51 -0500

    Fix links when searching spawns by NPC

--- templates/spawn/spawn.searchresults.tmpl.php
+++ templates/spawn/spawn.searchresults.tmpl.php
@@ -1,15 +1,13 @@
-      <div class="table_container" style="width:250px;">
-        <div class="table_header">
-          Spawn Search Results
-        </div>
-        <div class="table_content">
+  <div class="table_container" style="width:250px;">
+  <div class="table_header">Spawn Search Results</div>
+    <div class="table_content">
 <?if($results != ''):?>
 <?foreach($results as $result): extract($result);?>
-          <a href="index.php?editor=spawn&npcid=<?=$id?>&z=<?=get_zone_by_npcid($id)?>"><?=$name?> - (<?=get_zone_by_npcid($id)?>)</a><br>
+      <a href="index.php?editor=spawn&z=<?=get_zone_by_npcid($id)?>&zoneid=<?=getZoneIDByName(get_zone_by_npcid($id))?>&npcid=<?=$id?>"><?=$name?> - (<?=get_zone_by_npcid($id)?>)</a><br>
 <?endforeach;?>
 <?endif;?>
 <?if($results == ''):?>
-          Your search produced no results!
+      Your search produced no results!
 <?endif;?>
-        </div>
-      </div>
+    </div>
+  </div>
