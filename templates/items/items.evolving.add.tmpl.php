7d29cc7e0cf47674c0b4340be47af533b907e055
JJ <jeremiah.oligario@gmail.com>
2025-02-17 09:53:39 -0500

    Default values when adding evolving items

--- templates/items/items.evolving.add.tmpl.php
+++ templates/items/items.evolving.add.tmpl.php
@@ -7,19 +7,19 @@
             <td align="left"><strong>ID:</strong><br><input type="text" name="id" size="10" value="<?=$suggest_id?>"></td>
             <td align="left"><strong>Evolution ID:</strong><br><input type="text" name="item_evo_id" size="10" value="<?=$suggest_evo_id?>"></td>
             <td align="left"><strong>Level:</strong><br><input type="text" name="item_evolve_level" size="5" value="<?echo (isset($suggest_evo_level)) ? $suggest_evo_level : "1";?>"</td>
-            <td align="left"><strong>Item ID:</strong><br><input type="text" name="item_id" id="item_search" size="10" value=""></td>
+            <td align="left"><strong>Item ID:</strong><br><input type="text" name="item_id" id="item_search" size="10" value="0"></td>
           </tr>
           <tr>
             <td align="left">
               <strong>Type:</strong><br>
               <select name="type">
 <?foreach ($evolving_type as $k=>$v):?>
-                <option value="<?=$k?>"><?=$v?> (<?=$k?>)</option>
+                <option value="<?=$k?>"<?echo ($k == 99) ? " selected" : "";?>><?=$v?> (<?=$k?>)</option>
 <?endforeach;?>
               </select>
             </td>
-            <td align="left"><strong>Subtype:</strong><br><input type="text" name="sub_type" size="10" value=""></td>
-            <td align="left"><strong>Required Amt:</strong><br><input type="text" name="required_amount" size="10" value=""></td>
+            <td align="left"><strong>Subtype:</strong><br><input type="text" name="sub_type" size="10" value="1"></td>
+            <td align="left"><strong>Required Amt:</strong><br><input type="text" name="required_amount" size="10" value="0"></td>
             <td align="left">&nbsp;</td>
           </tr>
         </table><br>
