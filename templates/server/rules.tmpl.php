861297449ff06d5aa9203f798a33e608b0d9111d
JJ <jeremiah.oligario@gmail.com>
2025-03-30 18:59:13 -0400

    Fix table alignment in server rules editor

--- templates/server/rules.tmpl.php
+++ templates/server/rules.tmpl.php
@@ -1,4 +1,4 @@
-        <div class="table_container" style="width: 500px;">
+        <div class="table_container" style="width: 700px;">
           <div class="table_header">
             <table width="100%" cellpadding="0" cellspacing="0">
               <tr>
@@ -12,19 +12,19 @@
             <tr>
               <td align="center" width="60%"><strong>Name</strong></td>
               <td align="center" width="30%"><strong>Value</strong></td>
-              <td width="10%">&nbsp;</td>
+              <td width="10%" align="right">&nbsp;</td>
             </tr>
 <?$x=0; foreach($rules as $rules=>$v):?>
             <tr bgcolor="#<? echo ($x % 2 == 0) ? "BBBBBB" : "AAAAAA";?>">
-              <td align="center" width="20%"><?=$v['rule_name']?></td>
-              <td align="center" width="15%"><?=$v['rule_value']?></td>
-              <td align="right"><a href="index.php?editor=server&rule_name=<?=$v['rule_name']?>&ruleset_id=<?=$v['ruleset_id']?>&action=17"><img src="images/edit2.gif" border="0" title="Edit ruleset"></a>&nbsp;<a onClick="return confirm('Really Delete this Rule?');" href="index.php?editor=server&rule_name=<?=$v['rule_name']?>&ruleset_id=<?=$v['ruleset_id']?>&action=21"><img src="images/remove3.gif" border="0" title="Delete this rule"></a></td>
+              <td align="center" width="60%"><?=$v['rule_name']?></td>
+              <td align="center" width="30%"><?=$v['rule_value']?></td>
+              <td width="10%" align="right"><a href="index.php?editor=server&rule_name=<?=$v['rule_name']?>&ruleset_id=<?=$v['ruleset_id']?>&action=17"><img src="images/edit2.gif" border="0" title="Edit ruleset"></a>&nbsp;<a onClick="return confirm('Really Delete this Rule?');" href="index.php?editor=server&rule_name=<?=$v['rule_name']?>&ruleset_id=<?=$v['ruleset_id']?>&action=21"><img src="images/remove3.gif" border="0" title="Delete this rule"></a></td>
             </tr>
 <?$x++; endforeach;?>
 <?endif;?>
 <?if (!isset($rules)):?>
             <tr>
-              <td align="left" width="100" style="padding: 10px;">No rules</td>
+              <td align="left" width="100%" style="padding: 10px;">No rules</td>
             </tr>
 <?endif;?>
           </table>
