e4c8a62778ca730199b9ba6eeaac13a574d61597
JJ <jeremiah.oligario@gmail.com>
2025-05-03 23:10:38 -0400

    Adjust Player Event Log Settings display

--- templates/server/player.event.log.settings.view.tmpl.php
+++ templates/server/player.event.log.settings.view.tmpl.php
@@ -12,19 +12,19 @@
     <table class="table_content2" width="100%">
 <?if(isset($settings)):?>
       <tr>
-        <td align="center" width="20%"><strong>ID</strong></td>
-        <td align="center" width="45%"><strong>Event Name</strong></td>
+        <td align="center" width="10%"><strong>ID</strong></td>
+        <td align="center" width="50%"><strong>Event Name</strong></td>
         <td align="center" width="10%"><strong>Enabled</strong></td>
-        <td align="center" width="10%"><strong>Retention</strong></td>
+        <td align="center" width="15%"><strong>Retention</strong></td>
         <td align="center" width="10%"><strong>ETL</strong></td>
         <td width="10%">&nbsp;</td>
       </tr>
 <?$x=0; foreach($settings as $setting):?>
       <tr bgcolor="#<? echo ($x % 2 == 0) ? "BBBBBB" : "AAAAAA";?>">
-        <td align="center" width="20%"><?=$setting['id']?></td>
-        <td align="center" width="45%"><?=$setting['event_name']?></td>
+        <td align="center" width="10%"><?=$setting['id']?></td>
+        <td align="center" width="50%"><?=$setting['event_name']?></td>
         <td align="center" width="10%"><?=$yesno[$setting['event_enabled']]?></td>
-        <td align="center" width="10%"><?=$setting['retention_days']?></td>
+        <td align="center" width="15%"><?=$setting['retention_days']?></td>
         <td align="center" width="10%"><?=$yesno[$setting['etl_enabled']]?></td>
         <td align="right" width="10%">
           <a href="index.php?editor=server&id=<?=$setting['id']?>&action=79"><img src="images/edit2.gif" border="0" title="Edit Setting"></a>          
