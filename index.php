2a2d46c58f1bb174eb142e181acdea65e630c649
JJ <3617814+joligario@users.noreply.github.com>
2025-07-30 20:56:36 -0400

    No changes needed for https://github.com/EQEmu/Server/pull/4972

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <?php
 
-$current_revision = "20 July 2025";
-$current_db_version = 9325;
+$current_revision = "30 July 2025";
+$current_db_version = 9326;
 
 require_once("config.php");
 require_once("lib/logging.php");
